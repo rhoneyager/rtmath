7c19273c68194d921ef3fc2916607891fe520a6d
Ryan Honeyager <ryan@honeyager.info>
2011-06-09 22:27:32 +0000

    lbl friend class fix. Netbeans makes build.
    git-svn-id: file:///mnt/svn/rtmath@88 1d52797c-35fc-48e8-8e80-2db72aa187e2

---
+++ rtmath/rtmath-console-test/nbproject/Makefile-Debug.mk
@@ -0,0 +1,91 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc.exe
+CCC=g++.exe
+CXX=g++.exe
+FC=gfortran.exe
+AS=as.exe
+
+# Macros
+CND_PLATFORM=MinGW-Windows
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/rtmath-console-test.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-lm ../rtmath-base/dist/Debug/MinGW-Windows/librtmath-base.a ../rtmath/dist/Debug/MinGW-Windows/librtmath.a
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test.exe: ../rtmath-base/dist/Debug/MinGW-Windows/librtmath-base.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test.exe: ../rtmath/dist/Debug/MinGW-Windows/librtmath.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/rtmath-console-test.o: rtmath-console-test.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.cc) -g -Wall -MMD -MP -MF $@.d -o ${OBJECTDIR}/rtmath-console-test.o rtmath-console-test.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../rtmath-base && ${MAKE}  -f Makefile CONF=Debug
+	cd ../rtmath && ${MAKE}  -f Makefile CONF=Debug
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/rtmath-console-test.exe
+
+# Subprojects
+.clean-subprojects:
+	cd ../rtmath-base && ${MAKE}  -f Makefile CONF=Debug clean
+	cd ../rtmath && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
