29759ba08e003fb57968c2a99f5f3bdc245b268b
Ryan Honeyager <ryan@honeyager.info>
2011-06-21 21:52:06 +0000

    Finished typing the profiles by hand! Notices that mls.txt was actually trp.txt, and corrected it. Need to write atmosphere adding algorithm that adds gas concentrations and allows me to override the features of certain profiles, like multiplying the CO2 conc. by a factor, or something.
    git-svn-id: file:///mnt/svn/rtmath@133 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/debug_subversion.h
+++ rtmath/rtmath/debug_subversion.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define SUB_REV 118
-#define SUB_DATE "2011/06/19 22:40:29"
-#define SUB_WCRANGE "118"
+#define SUB_REV 127
+#define SUB_DATE "2011/06/20 00:37:29"
+#define SUB_WCRANGE "127"
 #define SUB_SOURCE "https://solar.met.fsu.edu/svn/rtmath/rtmath"
