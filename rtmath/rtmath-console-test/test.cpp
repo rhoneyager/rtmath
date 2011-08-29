d40492e947f28f19d5953efc68543da68b1b5b42
Ryan Honeyager <ryan@honeyager.info>
2011-08-29 05:08:27 +0000

    Commit before tallahassee return
    git-svn-id: file:///mnt/svn/rtmath@201 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath-console-test/test.cpp
+++ rtmath/rtmath-console-test/test.cpp
@@ -19,6 +19,7 @@
 
 #include "../rtmath/debug_mem.h"
 #include "../rtmath/damatrix_quad.h"
+#include "../rtmath/rayleigh.h"
 
 int main(int argc, char* argv[])
 {
@@ -29,10 +30,41 @@ int main(int argc, char* argv[])
 		
 		rtmath::debug::memcheck::enabled = false;
 		rtmath::daint::deg = 1; // For now, use only one degree of quadrature for testing
-
-		
 		fflush(stderr);
 
+
+		double alb = 0.1;
+		double tau = 1.0;
+		double mu = 1.0, mun = 0.0, phi = 0.3, phin = 0.1;
+		double x = 1.0;
+		std::complex<double> m;
+		m.real(1.33);
+		m.imag(0.001);
+		mapid mid(mu,mun,phi,phin);	
+
+		// Use the rayleigh-scattering case, for starters
+		// Need a layer with a phasefunction matrixop
+		rayleigh::rayleighPhaseFunc ray(x,m);
+		
+
+		shared_ptr<daInitLayer> iLa( new daInitLayer(ray,alb, tau, rtselec::R) );
+		// Special cast is needed to keep shared_ptr happy. Unfortunate.
+		// It also fixes the double free issue with daInitLayer cacheing, I think
+		shared_ptr<damatrix> iLaBase = static_pointer_cast<damatrix>(iLa); 
+		shared_ptr<matrixop> iLaeval = iLaBase->eval(mid);
+		cout << "iLa\n";
+		iLaeval->print();
+		iLaeval = iLaBase->eval(mid);
+		cout << "iLa\n";
+		iLaeval->print();
+
+		shared_ptr<damatrix> iLaDoubled = damatrix::op(iLaBase,iLaBase,MULT);
+		shared_ptr<matrixop> iLaDoubledeval = iLaDoubled->eval(mid);
+		cout << "iLaDoubled\n";
+		iLaDoubledeval->print();
+		iLaDoubledeval = iLaDoubled->eval(mid);
+		cout << "iLaDoubled\n";
+		iLaDoubledeval->print();
 		
 		cout << "Test program routines finished." << endl;
 #ifdef _WIN32
