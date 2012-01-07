72ad2c4a39a25367f66d6af19f7a6807410f6913
Ryan Honeyager <ryan@honeyager.info>
2012-01-07 21:57:22 +0000

    And another small error fixed
    git-svn-id: file:///mnt/svn/rtmath@285 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/rtmath/error/debug_subversion.h
+++ rtmath/rtmath/rtmath/error/debug_subversion.h
@@ -1,4 +1,4 @@
 #pragma once
-#define SUB_REV 283
-#define SUB_DATE "2012-01-07 16:50:45 -0500 (Sat, 07 Jan 2012)" 
+#define SUB_REV 284
+#define SUB_DATE "2012-01-07 16:54:26 -0500 (Sat, 07 Jan 2012)" 
 #define SUB_SOURCE "https://solar.met.fsu.edu/svn/rtmath/rtmath/rtmath" 
