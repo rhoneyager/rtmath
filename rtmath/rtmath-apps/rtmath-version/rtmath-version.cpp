dc28a11a4849c0ccb8a8eddfb3a00b9208a5a740
Ryan Honeyager <ryan@honeyager.info>
2012-01-09 06:56:35 +0000

    Created rtmath-version program
    git-svn-id: file:///mnt/svn/rtmath@295 1d52797c-35fc-48e8-8e80-2db72aa187e2

---
+++ rtmath/rtmath-apps/rtmath-version/rtmath-version.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+#include <rtmath/rtmath.h>
+
+int main(int argc, char* argv[])
+{
+	using namespace std;
+	using namespace rtmath;
+
+	rtmath::debug::debug_preamble();
+#ifdef _WIN32
+	std::getchar();
+#endif
+	return 0;
+}
