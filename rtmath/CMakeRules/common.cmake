cb1cc290bfcbe49b97f052c488440f90aa6788b5
Ryan Honeyager <ryan@honeyager.info>
2012-03-19 17:06:20 +0000

    CMakeRules should be really rtmath global.
    git-svn-id: file:///mnt/svn/rtmath@440 1d52797c-35fc-48e8-8e80-2db72aa187e2

---
+++ rtmath/CMakeRules/common.cmake
@@ -0,0 +1,28 @@
+# Common include file for misra project
+
+# Loads the necessary components for libraries and sets various 
+# compiler flags. 
+
+# Find the locations of ROOT, OpenMP and others
+include (FindROOT.cmake)
+include (FindOpenMP.cmake)
+
+include_directories (AFTER SYSTEM ${ROOT_INCLUDES})
+
+# Set libraries on unix (MSVC provides automatically)
+IF(${UNIX})
+SET (COMMON_LIBS netcdf m boost_filesystem boost_unit_test_framework boost_system ${ROOT_LIBRARIES})
+ENDIF()
+
+# Enable C++11
+IF(${CMAKE_COMPILER_IS_GNUCXX})
+SET (COMMON_CFLAGS ${COMMON_CFLAGS} -std=c++0x)
+ENDIF()
+
+# Take OpenMP option and enable / disable as appropriate
+IF(${USE_OPENMP})
+IF (${OPENMP_FOUND})
+SET (COMMON_CFLAGS ${COMMON_CFLAGS} ${OpenMP_CXX_FLAGS})
+ENDIF()
+ENDIF()
+
