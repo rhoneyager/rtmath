9fc55005de8b18d930b8e7b1937c025789ff29e6
Ryan Honeyager <ryan@honeyager.info>
2012-05-20 19:14:23 +0000

    Slowly modifying cmake rules to not use aux_source_directory.
    git-svn-id: file:///mnt/svn/rtmath@529 1d52797c-35fc-48e8-8e80-2db72aa187e2

---
+++ rtmath/rtmath/files_hdr.cmake
@@ -0,0 +1,34 @@
+
+
+set (hdrfiles
+	rtmath/absorb.h
+	rtmath/atmos.h
+	rtmath/atmoslayer.h
+	rtmath/cmake-settings.h.in
+	rtmath/command.h
+	rtmath/common_templates.h
+	rtmath/config.h
+	rtmath/coords.h
+	rtmath/defs.h
+	rtmath/emd.h
+	rtmath/enums.h
+	rtmath/interpolatable.h
+	rtmath/lbl.h
+	rtmath/macros.h
+	rtmath/mapid.h
+	rtmath/matrixop.h
+	rtmath/parsers.h
+	rtmath/pfFixed.h
+	rtmath/phaseFunc.h
+	rtmath/polynomial.h
+	rtmath/quadrature.h
+	rtmath/refract.h
+	rtmath/resource.h
+	rtmath/ROOT_functions.h
+	rtmath/ROOTlink.h
+	rtmath/rtmath.h
+	rtmath/Stdafx.h
+	rtmath/units.h
+	rtmath/zeros.h
+)
+
