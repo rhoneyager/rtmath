d08287008855476a56c60b4cf44733e97bcf3b45
Ryan Honeyager <ryan@honeyager.info>
2012-06-12 05:05:00 +0000

    cmake fixes for when tmatrix is added
    git-svn-id: file:///mnt/svn/rtmath@560 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/addapp.cmake
+++ rtmath/CMakeRules/addapp.cmake
@@ -4,19 +4,12 @@
 # Take the variable {appname}, and link libraries,
 # set properties and create an INSTALL target
 macro(addapp appname)
-#	target_link_libraries (${appname} rtmath ${COMMON_LIBS})
-
-#	set(${appname}_INCLUDE_DIRS ${rtmath_INCLUDES})
-#	get_target_property(${appname}_INCLUDE_DIRS ${appname} INCLUDE_DIRECTORIES)
 	set(${appname}_LIBRARIES "")
 	set(${appname}_INCLUDE_DIRS "")
-#	set(${appname}_LIBRARIES ${rtmath_LIBRARIES})
 
 	add_package_dependency(${appname} DEPENDS_ON rtmathDummy)
-#	message("${appname} --- ${${appname}_INCLUDE_DIRS}")
-#	message("${rtmath_INCLUDES}")
 	target_link_libraries (${appname} ${${appname}_LIBRARIES})
-IF(DEFINED COMMON_CFLAGS )
+IF(DEFINED COMMON_LIBS )
 	target_link_libraries (${appname} ${COMMON_LIBS})
 ENDIF()
 IF(DEFINED rtmath_INCLUDE_DIRS)
@@ -26,10 +19,8 @@ ENDIF()
 	include_directories(${CMAKE_CURRENT_BINARY_DIR})
 	include_directories(${${appname}_INCLUDE_DIRS})
 
-#	message("${COMMON_LIBS}")
-#	message("${${appname}_INCLUDE_DIRS}")
 
-	IF(DEFINED COMMON_CFLAGS )
+	IF(DEFINED COMMON_CFLAGS) 
 		set_target_properties(${appname} PROPERTIES COMPILE_FLAGS ${COMMON_CFLAGS})
 	ENDIF()
 
