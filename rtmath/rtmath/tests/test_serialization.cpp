b2e620af2b36718a400faa4f5caa91b85d201a8e
Ryan Honeyager <ryan@honeyager.info>
2012-07-07 15:05:36 +0000

    git-svn-id: file:///mnt/svn/rtmath@638 1d52797c-35fc-48e8-8e80-2db72aa187e2

---
+++ rtmath/rtmath/tests/test_serialization.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+#define BOOST_TEST_DYN_LINK
+#include "globals.h"
+#include <boost/test/unit_test.hpp>
+#include <boost/filesystem.hpp>
+#include <memory>
+
+#include "../rtmath/rtmath.h"
+
+using namespace rtmath;
+using namespace rtmath::ddscat;
+using namespace std;
+using namespace rtmath::config;
+using namespace boost::filesystem;
+
+
+BOOST_AUTO_TEST_SUITE(test_serialization);
+//BOOST_FIXTURE_TEST_SUITE(test_serialization, ddparloader);
+// globals::instance()->
+
+// Test that ddscat.par file can load
+BOOST_AUTO_TEST_CASE(serialization_basic)
+{
+}
+
+BOOST_AUTO_TEST_CASE(serialization_compression_list)
+{
+}
+
+BOOST_AUTO_TEST_CASE(serialization_compression_gzip)
+{
+}
+
+BOOST_AUTO_TEST_CASE(serialization_compression_zlib)
+{
+}
+
+BOOST_AUTO_TEST_CASE(serialization_compression_bzip2)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
