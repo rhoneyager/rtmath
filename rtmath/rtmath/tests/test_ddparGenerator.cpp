b381eeb8938b256fd0289615263295c0529fdc87
Ryan Honeyager <ryan@honeyager.info>
2012-07-08 04:31:37 +0000

    git-svn-id: file:///mnt/svn/rtmath@639 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/tests/test_ddparGenerator.cpp
+++ rtmath/rtmath/tests/test_ddparGenerator.cpp
@@ -4,6 +4,7 @@
 #include "globals.h"
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/shared_ptr.hpp>
 #include <memory>
 
 #include "../rtmath/rtmath.h"
@@ -13,6 +14,23 @@ BOOST_AUTO_TEST_SUITE(test_ddparGenerator);
 
 using namespace rtmath;
 using namespace std;
+using namespace boost;
+using namespace rtmath::debug;
+using namespace rtmath::ddscat;
+using namespace rtmath::units;
+
+// Test to construct ddparGenerator
+BOOST_AUTO_TEST_CASE(pargenerator_construct)
+{
+}
+
+// Test to prepare a sample ddparGenerator file
+
+// Read a known ddparGenerator file
+
+// Attempt to generate a batch of runs
+
+//
 
 BOOST_AUTO_TEST_SUITE_END();
 
