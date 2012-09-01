302aa5c9d30e3a441324c07106c5ddd436dd730d
Ryan Honeyager <ryan@honeyager.info>
2012-09-01 19:53:07 +0000

    addapp now has sans-rtmath dep macro
    git-svn-id: file:///mnt/svn/rtmath@753 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/addapp.cmake
+++ rtmath/CMakeRules/addapp.cmake
@@ -39,3 +39,25 @@ macro(add_header_files srcs)
     list(APPEND ${srcs} ${hds})
   endif()
 endmacro(add_header_files srcs)
+
+macro(addbasicapp appname)
+	set(${appname}_LIBRARIES "")
+	set(${appname}_INCLUDE_DIRS "")
+
+	target_link_libraries (${appname} ${${appname}_LIBRARIES})
+IF(DEFINED COMMON_LIBS )
+	target_link_libraries (${appname} ${COMMON_LIBS})
+ENDIF()
+	include_directories(${CMAKE_CURRENT_BINARY_DIR})
+	include_directories(${${appname}_INCLUDE_DIRS})
+
+
+	IF(DEFINED COMMON_CFLAGS) 
+		set_target_properties(${appname} PROPERTIES COMPILE_FLAGS ${COMMON_CFLAGS})
+	ENDIF()
+
+	INSTALL(TARGETS ${appname} RUNTIME DESTINATION bin COMPONENT Applications)
+	set_target_properties(${appname} PROPERTIES FOLDER "Apps")
+endmacro(addbasicapp appname)
+
+
