c871fcd45554a688a2238310820aecac1978bfaa
Ryan Honeyager <ryan@honeyager.info>
2012-09-06 07:33:17 +0000

    Almost done. Removed old files and began code cleanup.
    Next few commits will focus on redo of tmatrix runs, and getting the code into a linux-
    buildable condition again.
    
    
    git-svn-id: file:///mnt/svn/rtmath@764 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/rtmath/ddscat/ddOutputSingle.h
+++ rtmath/rtmath/rtmath/ddscat/ddOutputSingle.h
@@ -13,7 +13,7 @@
 #include "shapefile.h"
 #include "../da/daStatic.h"
 #include "../coords.h"
-#include "../depGraph.h"
+//#include "../depGraph.h"
 
 namespace rtmath {
 	
@@ -31,70 +31,52 @@ namespace rtmath {
 			virtual ~ddOutputSingle(); //
 
 			// Direct reading and writing of ddscat-formatted files (avg, sca and fml)
-			void readFile(const std::string &filename);
+			void readFile(const std::string &filename); //
 			void writeFile(const std::string &filename) const; //
 
-			void writeFML(std::ostream &out) const; //
+			void writeFML(std::ostream &out) const; // TODO: finish these
 			void writeSCA(std::ostream &out) const; //
 			void writeAVG(std::ostream &out) const; //
 			void writeStatTable(std::ostream &out) const; //
 
-			void readFML(std::istream &in);
-			void readSCA(std::istream &in);
+			//void readFML(std::istream &in);
+			//void readSCA(std::istream &in);
 			void readAVG(std::istream &in);
-			void readStatTable(std::istream &in);
+			void readStatTable(std::istream &in); //
 
-			size_t version() const;
-			void version(size_t);
+			size_t version() const; //
+			void version(size_t); //
+			double beta() const; //
+			double theta() const; //
+			double phi() const; //
+			double wave() const; //
+			double aeff() const; //
 
-			bool operator<(const ddOutputSingle &rhs) const;
+			bool operator<(const ddOutputSingle &rhs) const; //
 		protected:
-			size_t _version;
+			size_t _version; //
+			double _beta, _theta, _phi, _wave, _aeff; //
 			void _init(); //
+			//void _populateDefaults();
 			std::map< std::string, boost::shared_ptr<ddOutputSingleObj> >
 				_objMap;
 			std::vector<double> _statTable;
 			std::map< rtmath::coords::cyclic<double> , 
 				boost::shared_ptr<const ddscat::ddScattMatrix> >
 				_scattMatricesRaw;
-
-			typedef boost::bimap< std::string, 
-				boost::shared_ptr<rtmath::graphs::vertex> > vertexMap;
-			typedef rtmath::graphs::setWeakVertex vertexSet;
-			std::set<boost::shared_ptr<rtmath::graphs::vertex> >
-				_depsAVG, _depsFML, _depsSCA, _vertices;
-			vertexMap _vertexMap;
-			boost::shared_ptr<rtmath::graphs::graph> _graph;
-			// Return vertex mappings
-			void getVertices(vertexMap &mappings);
-			// Search for vertex by name
-			bool mapVertex(const std::string &idstr, boost::shared_ptr<rtmath::graphs::vertex> &vertex);
-			// create ordering and apply vertex actions to shapeConstraints
-			void update(const rtmath::graphs::setWeakVertex &fixed);
-			// create ordering and apply vertex actions based on known shapeConstraints
-			void update();
-			// Establish basic vertices and construct graph.
-			void _constructGraph();
-			// Convenient functions to create a vertex and assign a name in _vertexMap
-			virtual boost::shared_ptr<rtmath::graphs::vertex> 
-				_createVertex(const std::string &name, bool OR = false);
-			// Create vertex like with connect, but by parsing string
-			virtual boost::shared_ptr<rtmath::graphs::vertex>
-				_createVertex(const std::string &name, 
-				const std::string &target, const std::string &depends);
-			// - Name and add existing vertex (used with vertex::connect)
-			virtual boost::shared_ptr<rtmath::graphs::vertex> 
-				_createVertex(const std::string &name, boost::shared_ptr<rtmath::graphs::vertex> vert);
 		};
 
 		class ddOutputSingleObj
 		{
-		private:
+		public:
 			friend class ddOutputSingle;
 			ddOutputSingleObj();
 			virtual ~ddOutputSingleObj();
-			virtual void write(std::ostream &out) const = 0;
-			virtual void read(std::istream &in) = 0;
+			virtual void write(std::ostream &out) const {}
+			virtual void read(std::istream &in) {}
+			static void findMap(const std::string &line, std::string &res); //
+			static boost::shared_ptr<ddOutputSingleObj> constructObj
+				(const std::string &key);
 		};
 
 		enum stat_entries
--- rtmath/rtmath/src/ddOutputSingle.cpp
+++ rtmath/rtmath/src/ddOutputSingle.cpp
@@ -17,6 +17,110 @@
 #include "../rtmath/units.h"
 #include "../rtmath/quadrature.h"
 
+namespace
+{
+	class ddver : public ::rtmath::ddscat::ddOutputSingleObj
+	{
+	public:
+		ddver() { _version = 72; }
+		virtual ~ddver() {}
+		virtual void write(std::ostream &out)
+		{
+			out << " DDSCAT --- DDSCAT ";
+			if (_version == 72) out << "7.2.0 [12.02.11]";
+			else if (_version == 70) out << "7.0.7 [09.12.11]";
+			out << std::endl;
+		}
+		virtual void read(std::istream &in)
+		{
+			std::string lin;
+			std::getline(in,lin);
+			if (lin.find("7.2")!=std::string::npos) _version = 72;
+			if (lin.find("7.0")!=std::string::npos) _version = 70;
+		}
+		size_t _version;
+		size_t version() const { return _version; }
+		void version(size_t n) { _version = n; }
+	};
+	class ddstring : public ::rtmath::ddscat::ddOutputSingleObj
+	{
+	public:
+		ddstring() {}
+		virtual ~ddstring() {}
+		virtual void write(std::ostream &out)
+		{
+			out << s << std::endl;
+		}
+		virtual void read(std::istream &in)
+		{
+			std::getline(in,s);
+		}
+		std::string s;
+	};
+	class ddtarget : public ::rtmath::ddscat::ddOutputSingleObj
+	{
+	public:
+		ddtarget() {}
+		virtual ~ddtarget() {}
+		virtual void write(std::ostream &out)
+		{
+			out << " TARGET --- ";
+			out << s << std::endl;
+		}
+		virtual void read(std::istream &in)
+		{
+			std::string lin;
+			std::getline(in,lin);
+			size_t p = lin.find("--- ");
+			s = lin.substr(p+4);
+		}
+		std::string s;
+	};
+	class ddSval : public ::rtmath::ddscat::ddOutputSingleObj
+	{
+	public:
+		ddSval(const std::string &tail) {this->tail = tail;}
+		virtual ~ddSval() {}
+		virtual void write(std::ostream &out)
+		{
+			out << s << "--- " << tail << std::endl;
+		}
+		virtual void read(std::istream &in)
+		{
+			std::string lin;
+			std::getline(in,lin);
+			size_t p = lin.find("--- ");
+			s = lin.substr(0,p);
+		}
+		std::string s, tail;
+	};
+
+	template <class T>
+	class ddNval : public ::rtmath::ddscat::ddOutputSingleObj
+	{
+	public:
+		ddNval(size_t pos = 0, const std::string &head = "", const std::string &tail = "") {this->pos = pos; this->head = head; this->tail = tail;}
+		virtual ~ddNval() {}
+		virtual void write(std::ostream &out)
+		{
+			out << head << val << tail << std::endl;
+		}
+		virtual void read(std::istream &in)
+		{
+			std::string lin;
+			std::getline(in,lin);
+			std::istringstream ii(lin);
+			for (size_t i=0; i<pos; i++)
+				ii >> lin;
+			ii >> val;
+		}
+		size_t pos;
+		T val;
+		std::string head, tail;
+	};
+
+}
+
 namespace rtmath {
 	namespace ddscat {
 
@@ -46,6 +150,96 @@ namespace rtmath {
 			} else {
 				throw rtmath::debug::xBadInput(filename.c_str());
 			}
+		}
+
+		void ddOutputSingle::readStatTable(std::istream &in)
+		{
+			using namespace std;
+			string line;
+			// First line was detected by the calling function, so it is outside of the istream now.
+			//std::getline(in,line); // "          Qext       Qabs       Qsca      g(1)=<cos>  <cos^2>     Qbk       Qpha" << endl;
+			in >> line; // " JO=1: ";
+			for (size_t i=0; i< (size_t) QEXT2; i++)
+				in >> _statTable[i];
+			in >> line; // " JO=2: ";
+			for (size_t i=(size_t) QEXT2; i< (size_t) QEXTM; i++)
+				in >> _statTable[i];
+			in >> line; // " mean: ";
+			for (size_t i=(size_t) QEXTM; i< (size_t) QPOL; i++)
+				in >> _statTable[i];
+			in >> line // " Qpol= " 
+				>> _statTable[(size_t) QPOL] >> line; // "dQpha=";
+			in >> _statTable[(size_t) DQPHA];
+
+			std::getline(in,line); // "         Qsca*g(1)   Qsca*g(2)   Qsca*g(3)   iter  mxiter  Nsca";
+			in >> line; // " JO=1: ";
+			for (size_t i=(size_t) QSCAG11; i< (size_t) QSCAG12; i++)
+				in >> _statTable[i];
+			in >> line; // " JO=2: ";
+			for (size_t i=(size_t) QSCAG12; i< (size_t) QSCAG1M; i++)
+				in >> _statTable[i];
+			in >> line; // " mean: ";
+			for (size_t i=(size_t) QSCAG1M; i< (size_t) NUM_STAT_ENTRIES; i++)
+				in >> _statTable[i];
+		}
+
+		void ddOutputSingle::readFile(const std::string &filename)
+		{
+			// Look at extension of file
+			std::ifstream in(filename.c_str());
+			boost::filesystem::path p(filename);
+			boost::filesystem::path pext = p.extension();
+			if (pext.string() == ".sca")
+			{
+				//readSCA(in);
+			} else if (pext.string() == ".fml")
+			{
+				//readFML(in);
+			} else if (pext.string() == ".avg")
+			{
+				readAVG(in);
+			} else {
+				throw rtmath::debug::xBadInput(filename.c_str());
+			}
+		}
+
+		void ddOutputSingle::version(size_t nv)
+		{
+			auto obj = ddOutputSingleObj::constructObj("version");
+			auto objc = boost::shared_dynamic_cast<ddver>(obj);
+			objc->version(nv);
+			_objMap["version"] = obj;
+			_version = nv;
+		}
+
+		void ddOutputSingle::readAVG(std::istream &in)
+		{
+			using namespace std;
+			std::string lin;
+			bool headerDone = false;
+			while (!headerDone)
+			{
+				std::getline(in,lin);
+				if (lin.compare("Qext") != string::npos)
+				{
+					headerDone = true;
+					break;
+				}
+				string key;
+				ddOutputSingleObj::findMap(lin,key);
+				if (key == "")
+				{
+					cerr << "Unknown line: " << lin << endl;
+					continue;
+				}
+				auto obj = ddOutputSingleObj::constructObj(key);
+				istringstream ii(lin);
+				obj->read(ii);
+				_objMap[key] = obj;
+			}
+
+			readStatTable(in);
+			// Do Mueller matrix read
 		}
 
 		void ddOutputSingle::writeStatTable(std::ostream &out) const
@@ -309,95 +503,150 @@ namespace rtmath {
 		void ddOutputSingle::_init()
 		{
 			_version = 72;
-			_constructGraph();
+			_beta = 0;
+			_theta = 0;
+			_phi = 0;
+			_wave = 0;
+			_aeff = 0;
 		}
 		
 		ddOutputSingle::~ddOutputSingle()
 		{
 		}
 
-		void ddOutputSingle::_constructGraph()
+		size_t ddOutputSingle::version() const
 		{
-			/* Here, a dependency graph is constructed indicating the 
-			 * relationships between various quantities. This allows for 
-			 * file format interconversions and detecting whether 
-			 * such a change is even possible
-			 */
-			using namespace std;
-			using namespace rtmath::graphs;
-			_vertices.clear();
-			_vertexMap.clear();
-			_depsAVG.clear();
-			_depsFML.clear();
-			_depsSCA.clear();
-
-			const size_t varnames_size = 0; // TODO: get number
-			const string rawvarnames[] = {	// TODO: finish
-				"version",
-				"target_line",
-				"soln_meth",
-				"polarizability",
-				"shape",
-				"numdipoles",
-				"d/aeff",
-				"d",
-				"aeff",
-				"wavelength",
-				"k.aeff",
-				"Nambient",
-				"neps",
-				"tol"
-			};
-
-			std::set<std::string> varnames( rawvarnames, rawvarnames + varnames_size );
-
-			// Create vertices for end variables
-			for (auto it = varnames.begin(); it != varnames.end(); ++it)
-				_createVertex(*it, true);
-
-			// Create vertices representing function relationships
-			// This is a table that lists the name of the new node, the variable being calculated, 
-			// and the necessary dependencies. This is much cleaner than repeated copying / pasting, 
-			// and is much easier to read.
-			const size_t varmapnames_size = 54;
-			const std::string rawvarmapnames[varmapnames_size] = {
-				// name,					target,				dependencies
-				"DAEFF",					"d/aeff",			"aeff,d",
-				"T_DENS",					"density",			"temp",
-				"AEFF_V",					"volume",			"aeff",
-				"V_AEFF",					"aeff",				"volume",
-				"MASS_V__DENS",				"density",			"mass,volume",
-				"MASS_DENS__V",				"volume",			"mass,density",
-				"DENS_V__MASS",				"mass",				"density,volume",
-				"FREQ_TEMP__IREFR_R",		"irefr_r",			"freq,temp",
-				"FREQ_TEMP__IREFR_I",		"irefr_i",			"freq,temp"
-			};
-
-			for (size_t i=0; i< varmapnames_size; i = i + 3)
-			{
-				const string &name = rawvarmapnames[i];
-				const string &starget = rawvarmapnames[i+1];
-				const string &deps = rawvarmapnames[i+2];
-
-				_createVertex(name, starget, deps);
-			}
-			
-
-			// Create the graph from the vertices
-			_graph = boost::shared_ptr<graph>(new graph(_vertices));
+			return _version;
 		}
 
-		/*
-		bool ddOutputSingle::operator< (const ddOutputSingle &rhs) const
+		double ddOutputSingle::beta() const
 		{
-			if (_freq != rhs._freq) return _freq < rhs._freq;
-			if (_reff != rhs._reff) return _reff < rhs._reff;
+			return _beta;
+		}
+
+		double ddOutputSingle::theta() const
+		{
+			return _theta;
+		}
+
+		double ddOutputSingle::phi() const
+		{
+			return _phi;
+		}
+
+		double ddOutputSingle::wave() const
+		{
+			return _wave;
+		}
+
+		double ddOutputSingle::aeff() const
+		{
+			return _aeff;
+		}
+
+		bool ddOutputSingle::operator<(const ddOutputSingle &rhs) const
+		{
+			if (_wave != rhs._wave) return _wave < rhs._wave;
+			if (_aeff != rhs._aeff) return _aeff < rhs._aeff;
 			if (_beta != rhs._beta) return _beta < rhs._beta;
 			if (_theta != rhs._theta) return _theta < rhs._theta;
 			if (_phi != rhs._phi) return _phi < rhs._phi;
 			return false;
 		}
-		*/
+
+		boost::shared_ptr<ddOutputSingleObj> ddOutputSingleObj::constructObj(const std::string &key)
+		{
+			// Given the given key, construct the appropriate object.
+			// Used in the reading algorithms.
+			boost::shared_ptr<ddOutputSingleObj> res;
+
+			if (key == "version") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddver>(new ddver));
+			if (key == "target") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddtarget>(new ddtarget));
+			if (key == "solnmeth") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddSval>(new ddSval("method of solution") ));
+			if (key == "polarizability") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddSval>(new ddSval("prescription for polarizabilies") ));
+			if (key == "shape") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddSval>(new ddSval("shape") ));
+			if (key == "numdipoles") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<size_t> >(new ddNval<size_t>(0, "", " = NAT0 = number of dipoles") ));
+			if (key == "d/aeff") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(0, "", " = d/aeff for this target [d=dipole spacing]") ));
+			if (key == "d") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(0, "", " = d (physical units)") ));
+
+			if (key == "aeff") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "  AEFF= ", " = effective radius (physical units)") ));
+			if (key == "wave") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "  WAVE= ", " = wavelength (in vacuo, physical units)") ));
+			if (key == "k.aeff") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "K*AEFF= ", " = 2*pi*aeff/lambda") ));
+			if (key == "nambient") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "NAMBIENT= ", " = refractive index of ambient medium") ));
+
+			if (key == "neps") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+
+			if (key == "tol") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "   TOL= ", " = error tolerance for CCG method") ));
+			if (key == "a1tgt") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "a2tgt") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+
+			if (key == "navg") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "  NAVG= ", " = (theta,phi) values used in comp. of Qsca,g") ));
+			if (key == "kveclf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "kvectf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "incpol1lf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "incpol2lf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "incpol1tf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "incpol2tf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+
+			if (key == "betarange") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "thetarange") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "phirange") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "etasca") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(0, "", " = ETASCA = param. controlling # of scatt. dirs used to calculate <cos> etc.") ));
+			if (key == "avgnumori") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "avgnumpol") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+
+			if (key == "xtf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "ytf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+			if (key == "ztf") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddstring>(new ddstring));
+
+			if (key == "beta") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, " BETA = ", " = rotation of target around A1") ));
+			if (key == "theta") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, " THETA= ", " = angle between A1 and k") ));
+			if (key == "phi") res = boost::shared_dynamic_cast<ddOutputSingleObj>(boost::shared_ptr<ddNval<double> >(new ddNval<double>(1, "  PHI = ", " = rotation of A1 around k") ));
+
+			return res;
+		}
+
+		void ddOutputSingleObj::findMap(const std::string &line, std::string &res)
+		{
+			res = "";
+			//if (line.find("")!=std::string::npos) res = "";
+			if (line.find("DDSCAT ---")!=std::string::npos) res = "version";
+			if (line.find("TARGET ---")!=std::string::npos) res = "target";
+			if (line.find("--- method of solution")!=std::string::npos) res = "solnmeth";
+			if (line.find(" --- prescription for ")!=std::string::npos) res = "polarizability";
+			if (line.find("--- shape")!=std::string::npos) res = "shape";
+			if (line.find("NAT0")!=std::string::npos) res = "numdipoles";
+			if (line.find("= d/aeff for this target")!=std::string::npos) res = "d/aeff";
+			if (line.find("= d (physical units)")!=std::string::npos) res = "d";
+			if (line.find("effective radius (physical units)")!=std::string::npos) res = "aeff";
+			if (line.find("wavelength")!=std::string::npos) res = "wave";
+			if (line.find("K*AEFF")!=std::string::npos) res = "k.aeff";
+			if (line.find("NAMBIENT")!=std::string::npos) res = "nambient";
+			if (line.find("eps.=")!=std::string::npos) res = "neps";
+			if (line.find("TOL")!=std::string::npos) res = "tol";
+			if (line.find("target axis A1 in Target Frame")!=std::string::npos) res = "a1tgt";
+			if (line.find("target axis A2 in Target Frame")!=std::string::npos) res = "a2tgt";
+			if (line.find("NAVG")!=std::string::npos) res = "navg";
+			if (line.find("k vector (latt. units) in Lab Frame")!=std::string::npos) res = "kveclf";
+			if (line.find("k vector (latt. units) in TF")!=std::string::npos) res = "kvectf";
+			if (line.find("inc.pol.vec. 1 in LF")!=std::string::npos) res = "incpol1lf";
+			if (line.find("inc.pol.vec. 2 in LF")!=std::string::npos) res = "incpol2lf";
+			if (line.find("inc.pol.vec. 1 in TF")!=std::string::npos) res = "incpol1tf";
+			if (line.find("inc.pol.vec. 2 in TF")!=std::string::npos) res = "incpol2tf"; //
+			if (line.find("beta_min")!=std::string::npos) res = "betarange";
+			if (line.find("theta_min")!=std::string::npos) res = "thetarange";
+			if (line.find("phi_min")!=std::string::npos) res = "phirange";
+			if (line.find("ETASCA")!=std::string::npos) res = "etasca";
+			if (line.find("target orientations")!=std::string::npos) res = "avgnumori";
+			if (line.find("incident polarizations")!=std::string::npos) res = "avgnumpol";
+			if (line.find("xmin,xmax")!=std::string::npos) res = "xtf";
+			if (line.find("ymin,ymax")!=std::string::npos) res = "ytf";
+			if (line.find("zmin,zmax")!=std::string::npos) res = "ztf";
+			if (line.find("BETA")!=std::string::npos) res = "beta";
+			if (line.find("THETA")!=std::string::npos) res = "theta";
+			if (line.find("PHI")!=std::string::npos) res = "phi";
+		}
+
 		/*
 		void ddOutputSingle::writeEvans(std::ostream &out, double freq) const
 		{
