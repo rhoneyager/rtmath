800c9110c520657755e7a2f213c081d7ae76a8b4
Ryan Honeyager <ryan@honeyager.info>
2013-07-12 20:38:38 +0000

    git-svn-id: file:///mnt/svn/rtmath@1037 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/rtmath/Serialization/serialization_macros.h
+++ rtmath/rtmath/rtmath/Serialization/serialization_macros.h
@@ -4,16 +4,18 @@
 //#include <boost/archive/text_oarchive.hpp>
 //#include <boost/archive/text_iarchive.hpp>
 
-/// \def EXPORT(U,T) Defines the exportable types (txt, xml) for all rtmath objects. 
+/// \def EXPORT(U,T) 
+/// Defines the exportable types (txt, xml) for all rtmath objects. 
 /// Macro used in serialization source files.
 
 #define EXPORT(U,T) \
 	template void DLEXPORT_RTMATH U(::boost::archive::xml_oarchive &, T &, const unsigned int); \
 	template void DLEXPORT_RTMATH U(::boost::archive::xml_iarchive &, T &, const unsigned int);
 
-/// \def EXPORTTEMPLATE(U,T,W) Defines exportable types for template objects.
+/// \def EXPORTTEMPLATE(U,T,W) 
+/// Defines exportable types for template objects.
 /// It's odd structure is to avoid accidental specialization of all objects.
-/// \see rtmath::paramSet<T> for an example.
+/// \see rtmath::paramSet <T> for an example.
 /// \note This is generally used with nested template specializations, as MSVC 2012 
 /// and below have a compiler bug.
 
@@ -21,7 +23,8 @@
 	template void DLEXPORT_RTMATH U<::boost::archive::xml_oarchive, T>(::boost::archive::xml_oarchive &,W<T>&, const unsigned int); \
 	template void DLEXPORT_RTMATH U(::boost::archive::xml_iarchive &, W<T> &, const unsigned int);
 
-/// \def EXPORTINTERNAL(U) Is used when defining serializations that are defined internally 
+/// \def EXPORTINTERNAL(U) 
+/// Used when defining serializations that are defined internally 
 /// in a class (where boost::serialization::access is a friend). This is the preferred type of export.
 /// \note DLEXPORT_RTMATH tag is not needed, as it is defined in an exported class.
 
