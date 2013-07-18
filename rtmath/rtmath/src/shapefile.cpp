9857d9feac5397eed0ace127413d0d93032c924e
Ryan Honeyager <ryan@honeyager.info>
2013-07-18 05:48:18 +0000

    Shapefile should now be able to read compressed input.
    git-svn-id: file:///mnt/svn/rtmath@1058 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath/src/shapefile.cpp
+++ rtmath/rtmath/src/shapefile.cpp
@@ -94,96 +94,85 @@ namespace rtmath {
 			this->filename = fname;
 			string s(a, fsize);
 
+			
+			boost::iostreams::filtering_istream sin;
 			// s can contain either compressed or uncompressed input at this point.
 			if (cmeth.size())
-			{
-				boost::iostreams::filtering_istream sin;
 				prep_decompression(cmeth, sin);
-/// \todo Fix the compressed read stuff. Lots of compile errors
-				throw rtmath::debug::xUnimplementedFunction();
-				//sin.push(s);
-				std::ostringstream buffer;
-				/// \todo Check if rdbuf works on a boost iostreams object
-				//buffer << sin.rdbuf();
-				string suncompressed = buffer.str();
-				readString(suncompressed);
-			} else {
-				readString(s);
-			}
+			sin.push(s);
+			readString(sin);
 		}
 
 		void shapefile::readHeader(std::istream &in)
 		{
-		}
-
-		void shapefile::readString(const std::string &in)
-		{
-			// Since istringstream is so slow, I'm dusting off my old atof macros (in 
-			// macros.h). These were used when I implemented lbl, and are very fast.
 			using namespace std;
 			_init();
 
-			// First, do header processing
-			//boost::chrono::system_clock::time_point cstart = boost::chrono::system_clock::now();
-			size_t pend = 0;
-			//boost::chrono::system_clock::time_point cheaderm;
+			// Do header processing using istreams.
+			// The previous method used strings, but this didn't work with compressed reads.
+
+			// The header is seven lines long
+			for (size_t i=0; i<7; i++)
 			{
-				// Seek to the end of the header, and construct an istringstream for just the header
-				size_t pstart = 0;
-				for (size_t i=0; i<7; i++)
+				string lin;
+				std::getline(in,lin);
+				size_t posa = 0, posb = 0;
+				Eigen::Array3f *v = nullptr;
+				switch (i)
 				{
-					pstart = pend;
-					pend = in.find_first_of("\n", pend+1);
-					size_t posa = 0, posb = pstart;
-					Eigen::Array3f *v = nullptr;
-					switch (i)
+				case 0: // Title line
+					desc = lin;
+					break;
+				case 1: // Number of dipoles
 					{
-					case 0: // Title line
-						desc = string(in.data(),pend);
-						break;
-					case 1: // Number of dipoles
+						// Seek to first nonspace character
+						posa = lin.find_first_not_of(" \t\n", posb);
+						// Find first space after this position
+						posb = lin.find_first_of(" \t\n", posa);
+						size_t len = posb - posa;
+						numPoints = rtmath::macros::m_atoi(&(lin.data()[posa]),len);
+					}
+					break;
+				case 6: // Junk line
+				default:
+					break;
+				case 2: // a1
+				case 3: // a2
+				case 4: // d
+				case 5: // x0
+					// These all have the same structure. Read in three doubles, then assign.
+					{
+						if (2==i) v=&a1;
+						if (3==i) v=&a2;
+						if (4==i) v=&d;
+						if (5==i) v=&x0;
+						for (size_t j=0;j<3;j++)
 						{
 							// Seek to first nonspace character
-							posa = in.find_first_not_of(" \t\n", posb);
+							posa = lin.find_first_not_of(" \t\n,", posb);
 							// Find first space after this position
-							posb = in.find_first_of(" \t\n", posa);
+							posb = lin.find_first_of(" \t\n,", posa);
 							size_t len = posb - posa;
-							numPoints = rtmath::macros::m_atoi(&(in.data()[posa]),len);
+							(*v)(j) = (float) rtmath::macros::m_atof(&(lin.data()[posa]),len);
 						}
-						break;
-					case 6: // Junk line
-					default:
-						break;
-					case 2: // a1
-					case 3: // a2
-					case 4: // d
-					case 5: // x0
-						// These all have the same structure. Read in three doubles, then assign.
-						{
-							if (2==i) v=&a1;
-							if (3==i) v=&a2;
-							if (4==i) v=&d;
-							if (5==i) v=&x0;
-							for (size_t j=0;j<3;j++)
-							{
-								// Seek to first nonspace character
-								posa = in.find_first_not_of(" \t\n,", posb);
-								// Find first space after this position
-								posb = in.find_first_of(" \t\n,", posa);
-								size_t len = posb - posa;
-								(*v)(j) = (float) rtmath::macros::m_atof(&(in.data()[posa]),len);
-							}
-						}
-						break;
 					}
+					break;
 				}
-
-				latticePts.resize(numPoints,3);
-				latticePtsRi.resize(numPoints,3);
-				latticePtsStd.resize(numPoints,3);
-				latticePtsNorm.resize(numPoints,3);
 			}
 
+			latticePts.resize(numPoints,3);
+			latticePtsRi.resize(numPoints,3);
+			latticePtsStd.resize(numPoints,3);
+			latticePtsNorm.resize(numPoints,3);
+		}
+
+		void shapefile::read(std::istream &iin)
+		{
+			// Since istringstream is so slow, I'm dusting off my old atof macros (in 
+			// macros.h). These were used when I implemented lbl, and are very fast.
+			readHeader(iin);
+
+			using namespace std;
 			using namespace boost::accumulators;
 			accumulator_set<float, stats<
 				tag::min,
@@ -196,6 +185,8 @@ namespace rtmath {
 			// Load in the lattice points through iteration and macro.h-based double extraction
 			for (size_t i=0; i< numPoints; i++)
 			{
+				string in;
+				std::getline(iin,in);
 				auto crdsm = latticePts.block<1,3>(i,0);
 				auto crdsi = latticePtsRi.block<1,3>(i,0);
 				for (size_t j=0; j<7; j++)
