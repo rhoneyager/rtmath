0c31503f19da781d65387158e57b98cbcee0b7c7
Ryan Honeyager <ryan@honeyager.info>
2014-02-02 17:09:33 +0000

    Moved over the superior silo-writing code from my Project 2 from Navon.
    git-svn-id: file:///mnt/svn/rtmath@1232 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/plugins/silo/WritePoints.cpp
+++ rtmath/plugins/silo/WritePoints.cpp
@@ -3,6 +3,17 @@
 #define DB_USE_MODERN_DTPTR
 #include <silo.h>
 
+/// Namespace defining function mappings
+namespace {
+	template<class T>
+	DBdatatype getSiloDatatype() { throw; }
+
+	template<> DBdatatype getSiloDatatype<float>() { return DB_FLOAT; }
+	template<> DBdatatype getSiloDatatype<double>() { return DB_DOUBLE; }
+	template<> DBdatatype getSiloDatatype<int>() { return DB_INT; }
+	template<> DBdatatype getSiloDatatype<char>() { return DB_CHAR; }
+}
+
 namespace rtmath {
 	namespace plugins {
 		namespace silo {
@@ -29,13 +40,13 @@ namespace rtmath {
 					pts.col(1).data(), pts.col(2).data() };
 				DBPutPointmesh(db, name, 3, pcoords, numPoints, DB_FLOAT, optlist);
 				DBFreeOptlist(optlist);
-				
+
 				for (const auto &a : vals)
 				{
 					// Write the point var
 					optlist = DBMakeOptlist(1);
 					DBAddOption(optlist, DBOPT_UNITS, (void *)std::get<1>(a).c_str());
-					
+
 					const float **vals = new const float*[std::get<2>(a).cols()];
 					for (size_t i = 0; i < (size_t) std::get<2>(a).cols(); ++i)
 						vals[i] = std::get<2>(a).col(i).data();
@@ -50,6 +61,187 @@ namespace rtmath {
 					delete[] vals;
 				}
 			}
+
+
+
+			siloFile::siloFile(const char* filename, const char* desc)
+			{
+				df = DBCreate(filename, DB_CLOBBER, DB_LOCAL,
+					desc, // Optional string describing file
+					DB_PDB);
+				assert(df);
+			}
+
+			siloFile::~siloFile()
+			{
+				DBClose(df);
+			}
+
+
+			template <class T>
+			std::shared_ptr<siloFile::pointMesh<T> > siloFile::pointMesh<T>::createMesh(
+				std::shared_ptr<siloFile> parent,
+				const char* name, 
+				size_t ndims, size_t nCrds, const T* crdarray[],
+				const char *dimLabels[], const char *dimUnits[])
+			{
+				std::shared_ptr<pointMesh<T> > res(new pointMesh<T>(parent));
+				res->name = std::string(name);
+				res->numPoints = nCrds;
+
+				DBdatatype datatype = getSiloDatatype<T>();
+
+				DBoptlist *optlist = DBMakeOptlist((int)ndims*2);
+				if (dimLabels && dimUnits)
+					for (size_t i=0; i<ndims && i < 3; ++i)
+					{
+						const char *dimLabel = dimLabels[i];
+						const char *dimUnit = dimUnits[i];
+						res->dimLabels.push_back(std::string(dimLabel));
+						res->dimUnits.push_back(std::string(dimUnit));
+						int labelVal = DBOPT_XLABEL;
+						if (i==1) labelVal = DBOPT_YLABEL;
+						if (i==2) labelVal = DBOPT_ZLABEL;
+						int unitVal = DBOPT_XUNITS;
+						if (i==1) unitVal = DBOPT_YUNITS;
+						if (i==2) unitVal = DBOPT_ZUNITS;
+
+						DBAddOption(optlist, labelVal, (void*)dimLabel);
+						DBAddOption(optlist, unitVal, (void*)dimUnit);
+					}
+
+					//if (type == meshType::MESH_POINTS)
+					DBPutPointmesh(parent->df, name, (int) ndims, crdarray, (int) nCrds, datatype, optlist);
+					/*
+					else if (type == meshType::MESH_CURVILINEAR)
+					DBPutQuadmesh(df, name, NULL, crdarray, dims, 
+					ndims, datatype, DB_COLLINEAR, optlist);
+					*/
+					DBFreeOptlist(optlist);
+
+					return res;
+			}
+
+			template <class T>
+			void siloFile::pointMesh<T>::writeData(const char* varname,
+				const T** data, size_t nDims, const char* varUnits)
+			{
+				DBdatatype datatype = getSiloDatatype<T>();
+
+				DBoptlist *optlist = DBMakeOptlist(1);
+				if (varUnits)
+					DBAddOption(optlist, DBOPT_UNITS, (void *)varUnits);
+				DBPutPointvar(parent->df, varname, this->name.c_str(),
+					(int) nDims,
+					(void*) data,
+					(int) this->numPoints,
+					datatype,
+					optlist);
+				DBFreeOptlist(optlist);
+
+			}
+
+
+
+			template <class T>
+			std::shared_ptr<siloFile::rectilinearMesh<T> > siloFile::rectilinearMesh<T>::createMesh(
+				std::shared_ptr<siloFile> parent,
+				const char* name, 
+				size_t ndims, T* crdarray[], const int dimsizes[],
+				const char *dimLabels[], const char *dimUnits[])
+			{
+				std::shared_ptr<rectilinearMesh<T> > res(new rectilinearMesh<T>(parent));
+				res->name = std::string(name);
+				res->dimsizes.resize(ndims);
+				res->dimsizes.assign(dimsizes, dimsizes + ndims);
+				res->numPoints = 1;
+				for (const auto &it : res->dimsizes)
+					res->numPoints *= it;
+				//res->crdarray = crdarray;
+
+				DBdatatype datatype = getSiloDatatype<T>();
+
+				DBoptlist *optlist = DBMakeOptlist((int)ndims*2);
+				if (dimLabels && dimUnits)
+					for (size_t i=0; i<ndims && i < 3; ++i)
+					{
+						const char *dimLabel = dimLabels[i];
+						const char *dimUnit = dimUnits[i];
+						res->dimLabels.push_back(std::string(dimLabel));
+						res->dimUnits.push_back(std::string(dimUnit));
+						int labelVal = DBOPT_XLABEL;
+						if (i==1) labelVal = DBOPT_YLABEL;
+						if (i==2) labelVal = DBOPT_ZLABEL;
+						int unitVal = DBOPT_XUNITS;
+						if (i==1) unitVal = DBOPT_YUNITS;
+						if (i==2) unitVal = DBOPT_ZUNITS;
+
+						DBAddOption(optlist, labelVal, (void*)dimLabel);
+						DBAddOption(optlist, unitVal, (void*)dimUnit);
+					}
+
+					DBPutQuadmesh(parent->df, // dbfile
+						name, // mesh name
+						NULL, // coordinate names (ignored, set to NULL)
+						crdarray, // array of length ndims containing pointers to the coord arrays
+						res->dimsizes.data(), // array of length ndims describing mesh dimensionality
+						(int) ndims, // number of dimensions
+						datatype, // datatype of the coord arrays
+						DB_COLLINEAR, // rectilinear mesh
+						optlist // option list
+						);
+					DBFreeOptlist(optlist);
+
+					return res;
+			}
+
+			template <class T>
+			void siloFile::rectilinearMesh<T>::writeData(const char* varname,
+				const T** data, size_t nDims, const char* varunits,
+				const char** varnames
+				)
+			{
+				DBdatatype datatype = getSiloDatatype<T>();
+
+				DBoptlist *optlist = DBMakeOptlist(1);
+				if (varunits)
+					DBAddOption(optlist, DBOPT_UNITS, (void *)varunits);
+
+				char ** vn = const_cast<char**> (varnames);
+				const char* noname = varname;
+				std::vector<const char*> novarnames(nDims, noname);
+				if (!vn)
+					vn = const_cast<char**> (novarnames.data());
+
+				DBPutQuadvar(parent->df, varname, this->name.c_str(),
+					(int) nDims,
+					vn,
+					(void*) data,
+					//crdarray,
+					this->dimsizes.data(),
+					(int) this->dimsizes.size(),
+					NULL,
+					0,
+					datatype,
+					//DB_ZONECENT,
+					DB_NODECENT,
+					optlist
+					);
+				DBFreeOptlist(optlist);
+
+			}
+
+
+
+#define INST_MESHES(x) template class siloFile::mesh<x>; \
+	template class siloFile::pointMesh<x>; \
+	template class siloFile::rectilinearMesh<x>;
+
+			INST_MESHES(float);
+			INST_MESHES(double);
+			INST_MESHES(int);
+			INST_MESHES(char);
+
 
 		}
 	}
--- rtmath/plugins/silo/WritePoints.h
+++ rtmath/plugins/silo/WritePoints.h
@@ -1,4 +1,6 @@
+#include "../../rtmath/rtmath/defs.h"
 #include <array>
+#include <memory>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -18,13 +20,259 @@ namespace rtmath {
 			void writeShape(DBfile *f, const char* meshname, 
 				const rtmath::ddscat::shapefile::shapefile *shp);
 
-			void WritePoints(DBfile *db, 
+			/// \brief Old method of writing points to a silo file
+			/// \deprecated
+			void DEPRECATED WritePoints(DBfile *db, 
 				const char* meshname,
 				const std::array<std::string, 3> &axislabels,
 				const std::array<std::string, 3> &axisunits, 
 				const Eigen::Matrix<float, Eigen::Dynamic, 3> &pts,
 				std::vector<std::tuple<std::string, std::string, 
 				const Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> > > &vals);
+
+			
+			/**
+			* \brief Provides the ability to write various types of meshes and 
+			* data to silo files.
+			*
+			* This class opens a silo file for writing, and closes the file 
+			* upon destruction. Using the class members, various types of 
+			* meshes may be constructed. Each mesh supports various writing 
+			* methods for data storage.
+			*
+			* \todo Enable re-finding of meshes.
+			**/
+			class siloFile : public std::enable_shared_from_this<siloFile>
+			{
+			protected:
+				class meshBase : protected std::enable_shared_from_this<meshBase> {protected: meshBase() {} virtual ~meshBase() {}};
+				siloFile(const char* filename, const char* desc = nullptr);
+			private:
+				DBfile *df;
+				friend class meshBase;
+				//std::vector<std::weak_ptr<meshBase> > meshes;
+			public:
+				static std::shared_ptr<siloFile> generate
+					(const char* filename, const char* desc = nullptr)
+				{
+					return std::shared_ptr<siloFile>(new siloFile(filename, desc));
+				}
+				~siloFile();
+
+				enum class meshType
+				{
+					MESH_RECTILINEAR,
+					MESH_POINTS
+				};
+
+				template <class T>
+				class mesh : public meshBase
+				{
+				protected:
+					mesh(meshType type, std::shared_ptr<siloFile> parent) 
+						: type(type), parent(parent), numPoints(0) {}
+					meshType type;
+					std::string name;
+					std::vector<std::string> dimLabels;
+					std::vector<std::string> dimUnits;
+					std::shared_ptr<siloFile> parent;
+					size_t numPoints;
+				};
+				template <class T>
+				class pointMesh : public mesh<T>
+				{
+					friend class siloFile;
+				protected:
+					pointMesh(std::shared_ptr<siloFile> parent) 
+						: mesh(meshType::MESH_POINTS, parent) {}
+				public:
+					virtual ~pointMesh() {}
+					static std::shared_ptr<pointMesh<T> > createMesh(
+						std::shared_ptr<siloFile> parent,
+						const char* name, size_t ndims, size_t nCrds,
+						const T* crdarray[],
+						const char *dimLabels[] = nullptr,
+						const char *dimUnits[] = nullptr);
+					static std::shared_ptr<pointMesh<T> > createMesh(
+						std::shared_ptr<siloFile> parent,
+						const char* name,
+						const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &crds,
+						const char *dimLabels[] = nullptr, const char *dimUnits[] = nullptr
+						)
+					{
+						std::vector<const T*> crdarray(crds.rows());
+						//std::unique_ptr<T[]> crdarray(new T[crds.rows()]);
+						for (size_t i=0; i<(size_t)crds.cols(); ++i)
+							crdarray[i] = crds.col(i).data();
+						return createMesh(parent, name, crds.cols(), crds.rows(), 
+							crdarray.data(), dimLabels, dimUnits);
+					}
+
+					void writeData(const char* varname,
+						const T** data, size_t nDims, const char* varUnits = nullptr);
+
+					void writeData(const char* varname,
+						const T* data, const char* varUnits = nullptr)
+					{
+						const T * cdata[] = { data };
+						writeData(varname, cdata, 1, varUnits);
+					}
+				};
+
+				// Delegates for pointMesh
+				template<class T>
+				std::shared_ptr<pointMesh<T> > createPointMesh(
+					const char* name, size_t ndims, size_t nCrds,
+					const T* crdarray[],
+					const char *dimLabels[] = nullptr,
+					const char *dimUnits[] = nullptr)
+				{
+					return pointMesh<T>::createMesh(shared_from_this(),
+						name, ndims, nCrds, crdarray, dimLabels, dimUnits);
+				}
+				template<class T>
+				std::shared_ptr<pointMesh<T> > createPointMesh(
+					const char* name,
+					const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &crds,
+					const char *dimLabels[] = nullptr, const char *dimUnits[] = nullptr)
+				{
+					return pointMesh<T>::createMesh(shared_from_this(),
+						name, crds, dimLabels, dimUnits);
+				}
+
+
+				template <class T>
+				class rectilinearMesh : public mesh<T>
+				{
+					friend class siloFile;
+				protected:
+					rectilinearMesh(std::shared_ptr<siloFile> parent) 
+						: mesh(meshType::MESH_RECTILINEAR, parent) {}
+					std::vector<int> dimsizes;
+
+					//const T* crdarray[];
+				public:
+					virtual ~rectilinearMesh() {}
+					static std::shared_ptr<rectilinearMesh<T> > createMesh(
+						std::shared_ptr<siloFile> parent,
+						const char* name, size_t ndims, 
+						T* crdarray[],
+						const int dimsizes[],
+						const char *dimLabels[] = nullptr,
+						const char *dimUnits[] = nullptr);
+
+					/// Used for grids without coordinates (auto-generate indices)
+					static std::shared_ptr<rectilinearMesh<T> > createMesh(
+						std::shared_ptr<siloFile> parent,
+						const char* name, size_t ndims, 
+						const int dimsizes[],
+						const char *dimLabels[] = nullptr,
+						const char *dimUnits[] = nullptr)
+					{
+						T** crdarray = new T*[ndims];
+						//std::vector<T*> crdarray(ndims);
+						for (size_t i=0; i<ndims; ++i)
+						{
+							crdarray[i] = new T[dimsizes[i]];
+							for (size_t j=0; j<dimsizes[i]; ++j)
+								crdarray[i][j] = (T) j;
+						}
+						auto mesh = createMesh(parent, name, ndims, crdarray,
+							dimsizes, dimLabels, dimUnits);
+
+						for (size_t i=0; i<ndims; ++i)
+						{
+							delete[] crdarray[i];
+						}
+						delete[] crdarray;
+						return mesh;
+					}
+
+					void writeData(const char* varname,
+						const T** data, size_t nDims, 
+						const char *varunits = nullptr,
+						const char ** varNames = nullptr
+						);
+
+					void writeData(const char* varname,
+						const T* data, 
+						const char *varunits = nullptr,
+						const char ** varNames = nullptr
+						)
+					{
+						const T *cdata[] = { data };
+						writeData(varname, cdata, 1, varunits, varNames);
+					}
+				};
+
+				// Delegates for rectilinearMesh
+				template<class T>
+				std::shared_ptr<rectilinearMesh<T> > createRectilinearMesh(
+					const char* name, size_t ndims, const T* crdarray[],
+					const int dimsizes[],
+					const char *dimLabels[] = nullptr,
+					const char *dimUnits[] = nullptr)
+				{
+					return rectilinearMesh<T>::createMesh(shared_from_this(),
+						name, ndims, crdarray, dimsizes, dimLabels, dimUnits);
+				}
+
+				template<class T>
+				std::shared_ptr<rectilinearMesh<T> > createRectilinearMesh(
+					const char* name, size_t ndims, 
+					const int dimsizes[],
+					const char *dimLabels[] = nullptr,
+					const char *dimUnits[] = nullptr)
+				{
+					return rectilinearMesh<T>::createMesh(shared_from_this(),
+						name, ndims, dimsizes, dimLabels, dimUnits);
+				}
+
+
+
+				/*
+				/// \note dims is only used for quadmeshes
+				template <class T>
+				void createMesh(const char* name, meshType type, size_t ndims, 
+				size_t numCrds, const T* crdarray[], 
+				const int *dims = nullptr,
+				const char *dimLabels[] = nullptr, const char *dimUnits[] = nullptr);
+
+				template <class T>
+				void createMesh(const char* name, meshType type,
+				const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &crds,
+				const int *dims = nullptr,
+				const char *dimLabels[] = nullptr, const char *dimUnits[] = nullptr,
+				)
+				{
+				T* crdarray = new T[crds.rows()];
+				for (size_t i=0; i<crds.cols(); ++i)
+				crdarray[i] = crds.col(i).data();
+				createMesh<T>(name, type, crds.cols(), crds.rows(), crds.data(), 
+				dims, dimLabels, dimUnits);
+				delete[] crdarray;
+				}
+
+				template <class T>
+				void writeData(const char* meshname, const char* varname, 
+				size_t dimensionality, size_t numPoints, const T* data[],
+				const char* varUnits = nullptr);
+
+				template <class T>
+				void writeData(const char* varname, const char* meshname, 
+				meshType type,
+				const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &data,
+				const char* varUnits = nullptr)
+				{
+				T* dataarray = new T[crds.rows()];
+				for (size_t i=0; i<data.cols(); ++i)
+				dataarray[i] = data.col(i).data();
+				writeData<T>(meshname, varname, data.cols(), data.rows(), dataarray, varUnits);
+				delete[] dataarray;
+				}
+
+				*/
+			};
 		}
 	}
 }
