b4c1d53de62475943af2255c5d99b2c277a39d79
Ryan Honeyager <ryan@honeyager.info>
2014-05-02 05:29:51 +0000

    Small header fix (forgot to install)
    git-svn-id: file:///mnt/svn/ryan-debug@55 1f4cd2ca-bd95-4731-83df-565e53d4a655

--- upgrade.bat
+++ upgrade.bat
@@ -1,5 +1,4 @@
 @echo off
 echo Providing a command-line example of the script needed to upgrade
-rem vomus ?
-msiexec /i Ryan_Debug.msi ADDLOCAL=ALL REINSTALL=ALL REINSTALLMODE=omus
+msiexec /i Ryan_Debug.msi ADDLOCAL=ALL REINSTALL=ALL REINSTALLMODE=vomus
 
