6d23ee16f5dc082e317a223ff755352c63e1ebfb
Ryan Honeyager <ryan@honeyager.info>
2014-05-06 04:04:04 +0000

    Updates to CMakeRules, targeted at making addAssembly.cmake and addInstallDirs.cmake more complete. Disabled automatic manifest configuration, since a multi-dll manifest needs special handling.
    git-svn-id: file:///mnt/svn/rtmath@1394 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/addAssembly.cmake
+++ rtmath/CMakeRules/addAssembly.cmake
@@ -1,3 +1,5 @@
+include (signing)
+
 macro(addAssembly basename ) 
 	#MAJOR MINOR REVISION SVNREVISION INSTALL_BIN_DIR)
 	#message("${MAJOR} ${MINOR} ${REVISION} ${SVNREVISION}")
@@ -45,4 +47,102 @@ else()
 endif()
  
 endmacro(addAssembly basename )
+
+macro(implementAssembly basename targetname packagein)
+	# Set dll properties for assembly name
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES Release_ASSEMBLY ${ASSEMBLY_NAME_Release} )
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES MinSizeRel_ASSEMBLY ${ASSEMBLY_NAME_MinSizeRel} )
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES RelWithDebInfo_ASSEMBLY ${ASSEMBLY_NAME_RelWithDebInfo} )
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES Debug_ASSEMBLY ${ASSEMBLY_NAME_Debug} )
+
+	# Install script
+	configure_file("${CMAKE_CURRENT_SOURCE_DIR}/${packagein}"
+		"${CMAKE_CURRENT_BINARY_DIR}/${packagein}" @ONLY)
+
+	# Main DLL
+
+	# TODO: reenable when more config is added
+	#foreach(p Debug RelWithDebInfo MinSizeRel Release)
+	#	set(ASSEMBLY_NAME ${ASSEMBLY_NAME_${p}})
+	#	configure_file(
+	#		"Ryan.${basename}.Release.manifest.template"
+	#		"${CMAKE_CURRENT_BINARY_DIR}/${p}/Ryan.${basename}.Release.pre.manifest" @ONLY)
+	#endforeach()
+
+	
+	# Alternate SxS assembly formulation - the full manifest is embedded into the dll
+	#configure_file(
+	#	"Ryan.Debug.Release.manifest.single.template"
+	#	"${CMAKE_CURRENT_BINARY_DIR}/Ryan.Debug.Release.single.pre.manifest" @ONLY)
+
+	# Shared assembly redirect
+	set (PublisherManifestName "policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.manifest")
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES Release_PUBLISHER "policy.@MAJOR@.@MINOR@.${ASSEMBLY_NAME_Release}.manifest" )
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES MinSizeRel_PUBLISHER "policy.@MAJOR@.@MINOR@.${ASSEMBLY_NAME_MinSizeRel}.manifest" )
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES RelWithDebInfo_PUBLISHER "policy.@MAJOR@.@MINOR@.${ASSEMBLY_NAME_RelWithDebInfo}.manifest")
+	SET_TARGET_PROPERTIES( ${targetname} PROPERTIES Debug_PUBLISHER "policy.@MAJOR@.@MINOR@.${ASSEMBLY_NAME_Debug}.manifest" )
+	configure_file(
+		"Ryan.${basename}.Release.manifest.publisher.template"
+		"${CMAKE_CURRENT_BINARY_DIR}/${PublisherManifestName}" @ONLY)
+
+	# App snippets
+	set (MANIFEST_APP_SNIPPET_NAME "${CMAKE_CURRENT_BINARY_DIR}/Ryan.${basename}.Release.manifest.h")
+	configure_file(
+		"cpp/manifestref.h.in"
+		"${MANIFEST_APP_SNIPPET_NAME}" @ONLY)
+
+	
+	# Sign the dll file and the apps
+	add_custom_command(TARGET ${targetname} POST_BUILD
+		COMMAND echo $<CONFIGURATION> - Signing DLL
+		COMMAND cd $<CONFIGURATION>
+		COMMAND signtool.exe sign /t http://timestamp.verisign.com/scripts/timestamp.dll $<TARGET_FILE:${targetname}>
+		COMMAND echo Updating manifest with hashes and making CDF
+		COMMAND mt.exe -manifest Ryan.${basename}.Release.pre.manifest
+			-makecdfs 
+			-out:$<TARGET_PROPERTY:${targetname},$<CONFIGURATION>_ASSEMBLY>.manifest
+		COMMAND echo Making catalog
+		COMMAND echo makecat.exe -v $<TARGET_PROPERTY:${targetname},$<CONFIGURATION>_ASSEMBLY>.manifest.cdf
+		COMMAND makecat.exe -v $<TARGET_PROPERTY:${targetname},$<CONFIGURATION>_ASSEMBLY>.manifest.cdf
+		COMMAND echo Signing catalog
+		COMMAND signtool.exe sign /t http://timestamp.verisign.com/scripts/timestamp.dll 
+			 "$<TARGET_FILE_DIR:${targetname}>/$<TARGET_PROPERTY:${targetname},$<CONFIGURATION>_ASSEMBLY>.cat"
+		COMMAND cd ..
+		)
+	add_custom_target(Ryan_${basename}-Policy-Publish ALL
+		DEPENDS ${targetname}
+		COMMAND echo Creating the policy cdf
+		COMMAND echo mt.exe -manifest ${PublisherManifestName} -makecdfs 
+		#-out:${PublisherManifestName}
+		COMMAND mt.exe -manifest ${PublisherManifestName} -makecdfs 
+		#-out:${PublisherManifestName}
+		COMMAND echo Make the policy catalog
+		COMMAND echo makecat.exe policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.manifest.cdf
+		COMMAND makecat.exe policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.manifest.cdf
+		COMMAND echo Sign the policy catalog
+		COMMAND echo signtool.exe sign /t http://timestamp.verisign.com/scripts/timestamp.dll 
+			policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.cat
+		COMMAND signtool.exe sign /t http://timestamp.verisign.com/scripts/timestamp.dll 
+			policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.cat
+		)
+
+	# Add installation targets for the library manifest, the sample app manifest and the library catalog
+	INSTALL(FILES 
+		"${CMAKE_CURRENT_BINARY_DIR}/Release/Ryan.${basename}.Release.cat"
+		"${CMAKE_CURRENT_BINARY_DIR}/Release/Ryan.${basename}.Release.manifest"
+		"${CMAKE_CURRENT_BINARY_DIR}/${PublisherManifestName}"
+		"${CMAKE_CURRENT_BINARY_DIR}/policy.@MAJOR@.@MINOR@.Ryan.${basename}.Release.cat"
+		DESTINATION "${INSTALL_ASSEMBLY_DIR}" COMPONENT Libraries)
+
+	INSTALL(FILES
+		"${MANIFEST_APP_SNIPPET_NAME}"
+		DESTINATION "${INSTALL_INCLUDE_DIR}/${basename}" COMPONENT Headers)
+
+	INSTALL(FILES
+		"${CMAKE_CURRENT_BINARY_DIR}/${packagein}"
+		"${CMAKE_CURRENT_SOURCE_DIR}/upgrade.bat"
+		"${CMAKE_CURRENT_SOURCE_DIR}/deploy.bat"
+		DESTINATION "${INSTALL_INCLUDE_DIR}/.." COMPONENT Scripts)
+
+endmacro(implementAssembly basename targetname packagein)
 
