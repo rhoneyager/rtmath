879286e9281bb3efe5f3a759fbaac057dae68346
Ryan Honeyager <ryan@honeyager.info>
2014-05-19 18:23:59 +0000

    Linux does not sign
    git-svn-id: file:///mnt/svn/rtmath@1414 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/signing.cmake
+++ rtmath/CMakeRules/signing.cmake
@@ -1,13 +1,17 @@
+if (WIN32)
 set (TIMESTAMP_PROVIDER /t http://timestamp.verisign.com/scripts/timestamp.dll
 	CACHE STRING "Specifies the site used for timestamping signed code")
+endif()
 
 macro(signing appname )
-	add_custom_command(TARGET ${appname} POST_BUILD #${appname}_Signed
-		#COMMAND mt.exe -manifest ${subset} -outputresource:"$<TARGET_FILE:${appname}>"\;\#1
-		COMMAND signtool.exe sign ${TIMESTAMP_PROVIDER} $<TARGET_FILE:${appname}>
-		#DEPENDS ${appname}
-		#COMMENT "Appending manifest in app ${appname} with ${subset} and signing."
-		#WORKING_DIRECTORY "$<TARGET_FILE_DIR:${appname}>"
-		)
+	if (WIN32)
+		add_custom_command(TARGET ${appname} POST_BUILD #${appname}_Signed
+			#COMMAND mt.exe -manifest ${subset} -outputresource:"$<TARGET_FILE:${appname}>"\;\#1
+			COMMAND signtool.exe sign ${TIMESTAMP_PROVIDER} $<TARGET_FILE:${appname}>
+			#DEPENDS ${appname}
+			#COMMENT "Appending manifest in app ${appname} with ${subset} and signing."
+			#WORKING_DIRECTORY "$<TARGET_FILE_DIR:${appname}>"
+			)
+	endif()
 endmacro(signing appname )
 
