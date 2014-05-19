7d2944a4d577f01cb8dc3de75f0bf9cc1eb4880f
Ryan Honeyager <ryan@honeyager.info>
2014-05-19 03:02:21 +0000

    Created a basic voronoi windows project that installs the 3d voro++ library. Caveats with the current build system: - collectdirs.bat needs an override for the voro++ directory, as + signs are deprecated in keys. - deploy.bat signtool needs expansion of ; to a space when configuring.
    msi build for the x64 vs 2012 and 2013 targets.
    
    
    git-svn-id: file:///mnt/svn/rtmath@1408 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/signing.cmake
+++ rtmath/CMakeRules/signing.cmake
@@ -1,7 +1,10 @@
+set (TIMESTAMP_PROVIDER /t http://timestamp.verisign.com/scripts/timestamp.dll
+	CACHE STRING "Specifies the site used for timestamping signed code")
+
 macro(signing appname )
 	add_custom_command(TARGET ${appname} POST_BUILD #${appname}_Signed
 		#COMMAND mt.exe -manifest ${subset} -outputresource:"$<TARGET_FILE:${appname}>"\;\#1
-		COMMAND signtool.exe sign /t http://timestamp.verisign.com/scripts/timestamp.dll $<TARGET_FILE:${appname}>
+		COMMAND signtool.exe sign ${TIMESTAMP_PROVIDER} $<TARGET_FILE:${appname}>
 		#DEPENDS ${appname}
 		#COMMENT "Appending manifest in app ${appname} with ${subset} and signing."
 		#WORKING_DIRECTORY "$<TARGET_FILE_DIR:${appname}>"
