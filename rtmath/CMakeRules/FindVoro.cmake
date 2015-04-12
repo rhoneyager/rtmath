7e778cdc484b77e42aaf90909a35f06a1580b5cb
Ryan Honeyager <ryan@honeyager.info>
2015-04-12 21:01:31 +0000

    Small change for voronoi builds on windows. Need to remember to fix manifest issue with Ryan_Debug (maybe).
    git-svn-id: file:///mnt/svn/rtmath@1687 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/FindVoro.cmake
+++ rtmath/CMakeRules/FindVoro.cmake
@@ -4,29 +4,35 @@ if (VORO_INCLUDE_DIR AND VORO_LIBRARIES)
   set(VORO_FIND_QUIETLY TRUE)
 endif()
 
-# Include dir
-find_path(VORO_INCLUDE_DIR 
-	NAMES voro++.hh
-	PATH_SUFFIXES voro++ Voro voro
-)
+find_package(Voro QUIET NO_MODULE)
 
-# Library
-find_library(VORO_LIBRARY 
-  NAMES libvoro++.so
-)
+if (NOT Voro_FOUND)
 
-# handle the QUIETLY and REQUIRED arguments and set POSTGRESQL_FOUND to TRUE if 
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(VORO DEFAULT_MSG VORO_LIBRARY VORO_INCLUDE_DIR)
+	# Include dir
+	find_path(VORO_INCLUDE_DIR 
+		NAMES voro++.hh
+		PATH_SUFFIXES voro++ Voro voro ../../include/Voro
+		HINTS ENV Voro_DIR
+	)
 
-IF(VORO_FOUND)
-  SET( VORO_LIBRARIES ${VORO_LIBRARY} )
-ELSE(VORO_FOUND)
-  SET( VORO_LIBRARIES )
-ENDIF(VORO_FOUND)
+	# Library
+	find_library(VORO_LIBRARY 
+	  NAMES libvoro++.so
+	)
 
-# Lastly make it so that the POSTGRESQL_LIBRARY and VORO_INCLUDE_DIR variables
-# only show up under the advanced options in the gui cmake applications.
-MARK_AS_ADVANCED( VORO_LIBRARY VORO_INCLUDE_DIR )
+	# handle the QUIETLY and REQUIRED arguments and set POSTGRESQL_FOUND to TRUE if 
+	# all listed variables are TRUE
+	INCLUDE(FindPackageHandleStandardArgs)
+	FIND_PACKAGE_HANDLE_STANDARD_ARGS(VORO DEFAULT_MSG VORO_LIBRARY VORO_INCLUDE_DIR)
 
+	IF(VORO_FOUND)
+	  SET( VORO_LIBRARIES ${VORO_LIBRARY} )
+	ELSE(VORO_FOUND)
+	  SET( VORO_LIBRARIES )
+	ENDIF(VORO_FOUND)
+
+	# Lastly make it so that the POSTGRESQL_LIBRARY and VORO_INCLUDE_DIR variables
+	# only show up under the advanced options in the gui cmake applications.
+	MARK_AS_ADVANCED( VORO_LIBRARY VORO_INCLUDE_DIR )
+
+endif()
