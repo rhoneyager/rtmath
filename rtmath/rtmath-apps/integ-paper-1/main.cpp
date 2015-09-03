bb787bdf687889167fe0c318712add7af2e80a5a
Ryan Honeyager <ryan@honeyager.info>
2015-09-03 03:23:29 +0000

    The integration routine should work. Need to split beforehand based on frequency.
    git-svn-id: file:///mnt/svn/rtmath@1745 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath-apps/integ-paper-1/main.cpp
+++ rtmath/rtmath-apps/integ-paper-1/main.cpp
@@ -3,6 +3,7 @@
 // cross-sections. It is given the aeff_ice column and a range
 // of columns to integrate over, and it performs the integration
 // using the Sekhon and Srivastava [1970] size distribution.
+#define BOOST_SPIRIT_USE_PHOENIX_V3
 
 #include <iostream>
 #include <boost/filesystem.hpp>
@@ -11,12 +12,14 @@
 #include <boost/accumulators/accumulators.hpp>
 #include <boost/accumulators/statistics.hpp>
 #include <boost/accumulators/statistics/mean.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/spirit/include/karma.hpp>
-#include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_stl.hpp>
+//#include <boost/spirit/include/karma.hpp>
+//#include <boost/spirit/include/qi.hpp>
+//#include <boost/spirit/include/qi_string.hpp>
+//#include <boost/spirit/include/phoenix_core.hpp>
+//#include <boost/spirit/include/phoenix_operator.hpp>
+//#include <boost/spirit/include/phoenix_stl.hpp>
 #include <fstream>
 #include <complex>
 #include <set>
@@ -27,7 +30,12 @@
 #include <Ryan_Debug/splitSet.h>
 #include <Ryan_Debug/error.h>
 #include <Ryan_Debug/logging.h>
+#include <Ryan_Debug/macros.h>
 #include <boost/log/sources/global_logger_storage.hpp>
+#include "../../rtmath/rtmath/units.h"
+#include "../../rtmath/rtmath/conversions/convertLength.h"
+#include "../../rtmath/rtmath/common_templates.h"
+#include "../../rtmath/rtmath/config.h"
 #include "../../rtmath/rtmath/registry.h"
 #include "../../rtmath/rtmath/error/debug.h"
 
@@ -47,6 +55,8 @@ int main(int argc, char *argv[])
 	using namespace std;
 	using namespace rtmath::units::keywords;
 	using rtmath::units::convertLength;
+	using namespace rtmath::config;
+	using namespace rtmath;
 	try {
 		cerr << "rtmath-integ-paper-1" << endl;
 		const double pi = boost::math::constants::pi<double>();
@@ -56,18 +66,15 @@ int main(int argc, char *argv[])
 			config("Config options"), hidden("Hidden options"), oall("all options");
 
 		rtmath::debug::add_options(cmdline, config, hidden);
-		rtmath::ddscat::stats::shapeFileStats::add_options(cmdline, config, hidden);
-		//rtmath::ddscat::ddOutput::add_options(cmdline, config, hidden);
-		//rtmath::refract::add_options(cmdline, config, hidden);
 
 		cmdline.add_options()
 			("input,i", po::value<string>(), "The input file (csv format)")
 			("range", po::value<string>(), "The set of the columns to be integrated over")
-			("x", po::value<size_t>(), "The x column (aeff_water_um). Starts at 1.")
-			("keep,k", po::value<string>(), "Columns to keep")
-			("tag,t", po::value<string>(), "An identifying tag column (optional)")
+			("iceaeffum,x", po::value<size_t>(), "The x column (aeff_ice_um). Starts at 1.")
+			//("keep,k", po::value<string>(), "Columns to keep")
+			//("tag,t", po::value<string>(), "An identifying tag column (optional)")
 			("output,o", po::value<string>(), "Output file")
-			("r", po::value<string>(), "The rainfall rates to consider (in mm/hr)")
+			("rainrates,r", po::value<string>(), "The rainfall rates to consider (in mm/hr)")
 			("help,h", "produce help message")
 			;
 
@@ -92,132 +99,148 @@ int main(int argc, char *argv[])
 
 		if (vm.count("help") || vm.size() == 0) doHelp("");
 
-
-
-		ofstream out(string(oprefix).c_str());
-		// Output a header line
-		out << "Cross-Section Method\tIce Aeff (um)\tMax Diameter (mm)\t"
-			"Ice Volume (mm^3)\tFrequency (GHz)\t"
-			"Volume Fraction\tEffective Density (g/cm^3)\t"
-			"Temperature (K)\tHash\t"
-			"AR Method\tRefractive Index Method\tVolume Fraction Method\t"
-			"Aspect Ratio\tLambda (um)\tM_re\tM_im\t"
-			"Size Parameter\tRescale aeff\t"
-			"Theta\tBeta\tPhi\tg\tQabs\tQbk\tQext\tQsca" << std::endl;
-
-		std::cerr << "Doing " << runs.size() << " runs." << std::endl;
-		cout << "Meth\tg\tQabs\tQbk\tQext\tQsca" << std::endl;
-		size_t i=1;
-		// Iterate over all possible runs
-		mylog("Executing. There are " << runs.size() << " runs to process.");
-		for (const auto &r : runs)
-		{
-			ostringstream ofiless;
-			// the prefix is expected to be the discriminant for the rotational data
-			ofiless << oprefix << "-t-" << r.temp << "-f-" << r.freq
-				<< "-aeff-" << r.aeff << "-md-" << r.maxDiamFull
-				<< "-vfrac-" << r.fv << "-aspect-" << r.ar;
-			string ofile = ofiless.str();
-			mylog(ofile << " --- " << i << " / " << runs.size() << " " << r.refHash);
-			cout << ofile << " --- " << i << " / " << runs.size() << " " << r.refHash << endl;
-			++i;
-
-			const double sizep = 2. * pi * r.aeff / r.lambda;
-
-			using namespace rtmath::phaseFuncs;
-			pf_class_registry::orientation_type o = pf_class_registry::orientation_type::ISOTROPIC;
-			pf_class_registry::inputParamsPartial i;
-			i.aeff = r.aeff;
-			i.aeff_rescale = vm["scale-aeff"].as<bool>();
-			i.aeff_version = pf_class_registry::inputParamsPartial::aeff_version_type::EQUIV_V_SPHERE;
-			i.eps = r.ar;
-			i.m = r.m;
-			i.ref = r.refHash;
-			i.maxDiamFull = r.maxDiamFull;
-			//std::cerr << "i.m = r.m = " << i.m << std::endl;
-			i.rmeth = rmeth; // Yeah, only one refractive index method per program invocation is supported.
-			i.shape = pf_class_registry::inputParamsPartial::shape_type::SPHEROID;
-			i.vFrac = r.fv;
-
-			mylog("run aeff " << i.aeff << "\n\taeff_rescale " << i.aeff_rescale <<
-				"\n\taeff_version EQUIV_V_SPHERE\n\teps " << i.eps <<
-				"\n\tm " << i.m.real() << ", " << i.m.imag() <<
-				"\n\tref " << i.ref <<
-				"\n\tmaxDiam " << i.maxDiamFull <<
-				"\n\trmeth " << refractScaling <<
-				"\n\tshape SPHEROID" <<
-				"\n\tvFrac " << i.vFrac);
-
-			if (r.fv <= 0 || r.fv > 1.) {
-				std::cout << "\tCowardly refusing to calculate scattering for an invalid particle. See "
-					"the log for details (channel=density). Skipping to next one." << std::endl;
-				mylog("Error. r.fv is not in (0,1]. Skipping.");
-				continue;
+		vector<string> entries;
+		entries.reserve(1100000 * 30);
+		vector<string> header;
+		size_t numLineCols = 0;
+		// Read in everything as strings separated by commas.
+		string lin;
+		if (!vm.count("input")) doHelp("Input file needed");
+		if (!vm.count("range")) doHelp("Data columns needed");
+		if (!vm.count("rainrates")) doHelp("Need to specify rainfall rates");
+		if (!vm.count("iceaeffum")) doHelp("Need column number for iceaeffum");
+		if (!vm.count("output")) doHelp("Output file needed");
+		string srange = vm["range"].as<string>();
+		string srainrates = vm["rainrates"].as<string>();
+		size_t cice = vm["iceaeffum"].as<size_t>();
+		string output = vm["output"].as<string>();
+		string input = vm["input"].as<string>();
+		paramSet<size_t> crange(srange);
+		paramSet<double> rainrates(srainrates);
+		ifstream in(input.c_str());
+		size_t line=0;
+		while (in.good()) {
+			std::getline(in,lin);
+			if (!in.good()) break;
+			if (!lin.size()) continue;
+			if (line == 0) {
+				//parse_strings_commas(lin.begin(), lin.end(), header);
+				boost::split(header, lin, boost::is_any_of(",\t"));
+				numLineCols = header.size();
+			} else {
+				//parse_strings_commas(lin.begin(), lin.end(), entries);
+				vector<string> se;
+				boost::split(se, lin, boost::is_any_of(",\t"));
+				entries.insert(entries.end(), se.begin(), se.end());
 			}
-
-
-			pf_provider p(o, i);
-
-			pf_provider::resCtype res;
-			pf_class_registry::setup s;
-			s.beta = 0; s.theta = 0; s.phi = 0;
-			s.sPhi = 0; s.sPhi0 = 0; s.sTheta = 0; s.sTheta0 = 0;
-			s.wavelength = r.lambda;
-
-			// smeth is parameter solution-method, which can force a single method to be used
-			p.getCrossSections(s, res, smeth);
-
-
-			double maxDiam = r.maxDiamFull, Vi = 0, effDen = 0; // V=0
-			{
-				using namespace rtmath::density;
-				//double diammm = maxDiam / 1000.;
-				//convertLength( _ar = r.ar,
-				//	_in_length_units = "um",
-				//	_out_length_units = "mm",
-				//	_in_length_value = maxDiam,
-				//	_in_length_type = "Max_Diameter_Full",
-				//	_out_length_type = "Max_Diameter_Full"
-				//	);
-				//V = (4. * pi / 3.) * pow(diammm/2.,3.);
-				Vi = (4. * pi / 3.) * pow(r.aeff/1000,3.);
-				//Vi = V;
-				//V /= r.fv;
-				//double rad = pow(3.*V/(4.*pi),1./3.);
-				//maxDiam = 2. * rad;
-
-				double dIce = ice1h( _temperature = r.temp, _temp_units = "K" );
-				effDen = dIce * r.fv;
-			}
-
-			for (const auto &rr : res)
-			{
-				out << rr.first << "\t" << r.aeff << "\t" << maxDiam / 1000. << "\t" 
-					<< Vi << "\t"
-					<< r.freq << "\t" << r.fv << "\t" << effDen << "\t" << r.temp << "\t"
-					<< r.refHash << "\t" << armeth << "\t" << refractScaling << "\t" 
-					<< r.fvMeth << "\t" //<< r.fvMeth * 0 << "\t" // get ice density here.....
-					<< r.ar << "\t" << r.lambda << "\t" << r.m.real() << "\t" << r.m.imag() << "\t"
-					<< sizep << "\t" << i.aeff_rescale << "\t"
-					<< s.theta << "\t" << s.beta << "\t" << s.phi << "\t"
-					<< rr.second.g_iso << "\t"
-					<< rr.second.Qabs_iso << "\t"
-					<< rr.second.Qbk_iso << "\t"
-					<< rr.second.Qext_iso << "\t"
-					<< rr.second.Qsca_iso
-					<< std::endl;
-
-				cout << rr.first << "\t"
-					<< rr.second.g_iso << "\t"
-					<< rr.second.Qabs_iso << "\t"
-					<< rr.second.Qbk_iso << "\t"
-					<< rr.second.Qext_iso << "\t"
-					<< rr.second.Qsca_iso
-					<< std::endl;
-			}
-			
+			line++;
 		}
 
+		size_t contentLines = line - 1;
+		// Assume that the data is ordered by effective radius. Assume frequencies are
+		// in separate files.
+		vector<double> ranges( (line - 1) * crange.size());
+		vector<double> PDFs( (line-1) * crange.size() * rainrates.size());
+		vector<double> CDFs( (line-1) * crange.size() * rainrates.size());
+		vector<double> diams( (line-1) ), iceaeffs(line-1);
+		auto pIndex = [&crange, &rainrates, &contentLines](size_t i, size_t ln, size_t nrr) -> size_t {
+			size_t res = (rainrates.size() - nrr - 1);
+			res *= crange.size();
+			res += (crange.size() - i - 1);
+			res *= contentLines;
+			res += ln;
+			return res;
+			//return i + (crange.size() * ln)
+			//		+ (rainrates.size() * crange.size() * ln);
+		};
+		//vector<string> rangesNames(crange.size());
+		// Convert source strings. Use my macros for speed.
+		std::cerr << "Total lines: " << line << ", Content lines: " << contentLines << std::endl
+			<< "numLineCols: " << numLineCols << ", crange.size(): " << crange.size() << std::endl
+			<< "ranges.size(): " << ranges.size() << ", diams.size(): " << diams.size() << std::endl;
+		std::cerr << "PDFs.size(): " << PDFs.size() << std::endl;
+		for (size_t ln = 0; ln < contentLines; ln++) {
+			size_t base = numLineCols * ln;
+			std::cout << "Line " << ln << ", base " << base << std::endl;
+			// Aeff
+			double aeff_ice = Ryan_Debug::macros::m_atof<double>(
+				entries.at(base+cice-1).c_str());
+			double mass = 0.9182 * pi * pow(aeff_ice,3.) * 4./3.;
+			double vwater = mass / 0.9981;
+			double aeff_water = pow(3.*vwater/(4.*pi),1./3.);
+			double deff_water = 2. * aeff_water;
+			diams.at(ln) = deff_water;
+			iceaeffs.at(ln) = aeff_ice;
+			// Sources
+			size_t i=0;
+			for (auto it = crange.begin(); it != crange.end(); ++it) {
+				double src = Ryan_Debug::macros::m_atof<double>(entries.at(base-1+(*it)).c_str());
+				size_t baseOut = crange.size() * ln;
+				ranges.at(baseOut + i) = src;
+
+				size_t nrr = 0;
+				for (auto rr = rainrates.begin(); rr != rainrates.end(); ++rr) {
+					double lambda = 22.9 * pow(*rr,-0.45);
+					double N0 = 2500. * pow(*rr,-0.94);
+					double Nd = N0 * exp(-1. * lambda * deff_water / 1.e4);
+					double pdf = Nd  * src;
+					std::cout << "at line " << ln << " and i " << i
+						<< " and nrr " << nrr << ", pIndex is "
+						<< pIndex(i,ln,nrr) << " (diam " << diams.at(ln)
+						<< " lam " << lambda
+						<< " N0 " << N0 << " Nd " << Nd
+						<< " src " << src << ")" << std::endl;
+					std::cout << "\tpdf is " << pdf;
+
+					PDFs.at(pIndex(i,ln,nrr)) = pdf;
+					if (ln > 0) {
+						// Just use the trapeziod rule.
+						double dw = deff_water - diams.at(ln-1);
+						double pdfprev = PDFs.at(pIndex(i,ln-1,nrr));
+						CDFs.at(pIndex(i,ln,nrr)) = CDFs.at(pIndex(i,ln-1,nrr))
+							+ ((pdf + pdfprev) * dw / 2.);
+						std::cout << "\tdw: " << dw << " prevPDF: " << pdfprev
+							<< " prevCDF " << CDFs.at(pIndex(i,ln-1,nrr))
+							<< " PCDF is " << CDFs.at(pIndex(i,ln,nrr));
+					}
+					std::cout << std::endl;
+					nrr++;
+				}
+				++i;
+			}
+
+		}
+		ofstream out(output.c_str());
+		// Write the header
+		out << "r";
+		for (auto it = crange.begin(); it != crange.end(); ++it) {
+			out << ",i" << header.at((*it)-1);
+		}
+		out << std::endl;
+		// Write the values
+		size_t nrr = 0;
+		for (auto rr = rainrates.begin(); rr != rainrates.end(); ++rr) {
+			out << *rr;
+			for (size_t i=0; i<crange.size(); ++i) {
+				out << "," << CDFs.at(pIndex(i, contentLines-1, nrr));
+			}
+			out << std::endl;
+			nrr++;
+		}
+		/*
+		out << "Aeff_ice,Deff_water";
+		for (auto it = crange.begin(); it != crange.end(); ++it) {
+			out << "," << header.at((*it)-1);
+		}
+		out << std::endl;
+		// Write the values
+		for (size_t i=0; i<diams.size(); ++i) {
+			out << iceaeffs.at(i) << "," << diams.at(i);
+			for (size_t j=0; j < crange.size(); ++j)
+				out << "," << ranges.at(j+(i*crange.size()));
+			out << std::endl;
+		}
+		*/
 	}
 	catch (std::exception &e) {
 		cerr << "Exception: " << e.what() << endl;
