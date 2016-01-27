806d52d38d308cd73f5ed9821241a25a2999cb49
Ryan Honeyager <ryan@honeyager.info>
2016-01-27 21:35:38 +0000

    Retargeting CMakeRules for trunk.
    git-svn-id: file:///mnt/svn/rtmath@1786 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/CMakeRules/manifest_append.cmake
+++
@@ -1,9 +0,0 @@
-macro(manifest_append appname subset)
-	add_custom_command(TARGET ${appname} #${appname}_Manifest_Append
-		COMMAND mt.exe -manifest ${subset} -outputresource:"$<TARGET_FILE:${appname}>"\;\#1
-		# -inputresource:"$<TARGET_FILE:${appname}>"\;\#1 
-		DEPENDS ${appname}
-		COMMENT "Appending manifest in app ${appname} with ${subset}"
-		)
-endmacro(manifest_append appname subset)
-
