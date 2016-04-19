613d95c61878f824d3c95e60ea71929e46f168b1
Ryan Honeyager <ryan@honeyager.info>
2016-04-19 04:11:19 +0000

    Oneellip-table implemented
    git-svn-id: file:///mnt/svn/rtmath@1838 1d52797c-35fc-48e8-8e80-2db72aa187e2

--- rtmath/rtmath-apps/oneellip-table/main.cpp
+++ rtmath/rtmath-apps/oneellip-table/main.cpp
@@ -58,13 +58,13 @@ int main(int argc, char *argv[])
 
 		runmatch.add_options()
 			("input,i", po::value<string>(), "Specify input file")
-			("col-md", po::value<int>()->default_value(3),
+			("col-md", po::value<int>()->default_value(6),
 			 "Specify maximum dimension column")
-			("col-aeff", po::value<int>()->default_value(2),
+			("col-aeff", po::value<int>()->default_value(5),
 			 "Specify effective radius column")
-			("col-ar", po::value<int>()->default_value(5),
+			("col-ar", po::value<int>()->default_value(8),
 			 "Specify aspect ratio column")
-			("col-vf", po::value<int>()->default_value(9),
+			("col-vf", po::value<int>()->default_value(12),
 			 "Specify volume fraction column")
 			("col-hash", po::value<int>()->default_value(1),
 			 "Specify hash column")
@@ -79,8 +79,6 @@ int main(int argc, char *argv[])
 			"(Sihvola, Debye, Maxwell_Garnett_Spheres, Maxwell_Garnett_Ellipsoids, Bruggeman). "
 			"Only matters if volume fractions are given. Then, default is Maxwell_Garnett_Ellipsoids.")
 			("temps,T", po::value<std::string>()->default_value("263"), "Specify temperatures in K")
-			("mr", po::value<double>(), "Override real refractive index value")
-			("mi", po::value<double>(), "Override imaginary refractive index value")
 			("solution-method", po::value<std::string>()->default_value("bhmie"), "Force only a specific algorithm to be used, such as "
 			 "Rayleigh or bhmie. No other pf generator will be used.")
 			;
@@ -120,6 +118,8 @@ int main(int argc, char *argv[])
 
 		if (vm.count("help") || vm.size() == 0) doHelp("");
 
+		string tag;
+		if (vm.count("tag")) tag = vm["tag"].as<string>();
 		string smeth;
 		if (vm.count("solution-method"))
 			smeth = vm["solution-method"].as<string>();
@@ -144,44 +144,12 @@ int main(int argc, char *argv[])
 			}
 		}
 
-		double dSpacing = 0;
-		if (vm.count("dipole-spacing"))
-			dSpacing = vm["dipole-spacing"].as<double>();
-
-		struct run
-		{
-			double temp, freq, aeff, lambda, ar;
-			double fv, maxDiamFull;
-			std::string fvMeth;
-			std::complex<double> m;
-			std::string refHash;
-			std::string tag;
-		};
-		vector<run> runs;
-		runs.reserve(50000);
-
 		// First, set any overrides for the frequency, temperature and refractive index
 		set<double> temps, freqs;
 		if (vm.count("temps"))
 			Ryan_Debug::splitSet::splitSet(vm["temps"].as<string>(), temps);
 		if (vm.count("freqs"))
 			Ryan_Debug::splitSet::splitSet(vm["freqs"].as<string>(), freqs);
-		bool overrideM = false;
-		complex<double> ovM;
-		if (vm.count("mr") || vm.count("mi"))
-		{
-			overrideM = true;
-			if (!vm.count("mr") || !vm.count("mi"))
-				doHelp("When overriding refractive index, need to specify "
-				"both real and imaginary components.");
-			double r = vm["mr"].as<double>();
-			double i = vm["mi"].as<double>();
-			ovM = complex<double>(r, i);
-		}
-		auto fixedBinder = [&](std::complex<double>, std::complex<double>, double, std::complex<double> &Mres)
-		{
-			Mres = ovM;
-		};
 
 		string refractScaling = vm["refract-method"].as<string>();
 		std::function<void(std::complex<double>, std::complex<double>, double, std::complex<double> &)> rmeth;
@@ -189,21 +157,65 @@ int main(int argc, char *argv[])
 		else if (refractScaling == "Maxwell_Garnett_Spheres") rmeth = rtmath::refract::maxwellGarnettSpheres;
 		else if (refractScaling == "Debye") rmeth = rtmath::refract::debyeDry;
 		else if (refractScaling == "Bruggeman") rmeth = rtmath::refract::bruggeman;
-		else if (!rescaleM) rmeth = fixedBinder;
 		else doHelp("Need to specify a proper refractive scaling.");
 
 		string armeth = vm["ar-method"].as<string>(); // Used much further below
 
+		double dSpacing = 0;
+		if (vm.count("dipole-spacing"))
+			dSpacing = vm["dipole-spacing"].as<double>();
+
+		int colmd = -1, colaeff = -1, colar = -1,
+			colvf = -1, colhash = -1;
+		colmd = vm["col-md"].as<int>();
+		colaeff = vm["col-aeff"].as<int>();
+		colar = vm["col-ar"].as<int>();
+		colvf = vm["col-vf"].as<int>();
+		colhash = vm["col-hash"].as<int>();
+		colmd--; colaeff--; colar--; colvf--; colhash--;
+		struct run
+		{
+			double temp, freq, aeff, ar;
+			double fv, maxDiamFull;
+			std::string refHash;
+		};
+		vector<run> runs;
+		runs.reserve(50000);
+
+		if (!vm.count("input")) doHelp("Must specify an input file");
+		string sinput = vm["input"].as<string>();
+		ifstream in(sinput.c_str());
+		string lin;
+		std::getline(in,lin); // ignore the header line
+		while(in.good()) {
+			std::getline(in,lin);
+			if (!lin.size()) continue;
+			// Explode tabs
+			vector<string> spstr;
+			Ryan_Debug::splitSet::splitVector(lin, spstr, '\t');
+			for (const auto &f : freqs)
+				for (const auto &t : temps) {
+					run r;
+					r.temp = t; r.freq = f;
+					r.aeff = boost::lexical_cast<double>(spstr.at(colaeff));
+					r.maxDiamFull = boost::lexical_cast<double>(spstr.at(colmd));
+					r.fv = boost::lexical_cast<double>(spstr.at(colvf));
+					r.refHash = spstr.at(colhash);
+					r.ar = boost::lexical_cast<double>(spstr.at(colar));
+					runs.push_back(std::move(r));
+				}
+		}
+
 		ofstream out(string(oprefix).c_str());
 		// Output a header line
-		out << "Cross-Section Method\tIce Aeff (um)\tMax Diameter (mm)\t"
+		out << "Cross-Section Method\tTag\t"
+			"Ice Aeff (um)\tMax Diameter (mm)\t"
 			"Ice Volume (mm^3)\tFrequency (GHz)\t"
 			"Volume Fraction\tEffective Density (g/cm^3)\t"
 			"Temperature (K)\tHash\t"
-			"AR Method\tRefractive Index Method\tVolume Fraction Method\t"
-			"Aspect Ratio\tLambda (um)\tM_re\tM_im\t"
-			"Size Parameter\t"
-			"Theta\tBeta\tPhi\tg\t";
+			"AR Method\t"
+			"Aspect Ratio\t"
+			"Size Parameter\tg\t";
 		if (vm.count("show-qs"))
 			out << "Qabs\tQbk\tQext\tQsca" << std::endl;
 		else out << "Cabs (m^2)\tCbk (m^2)\tCext (m^2)\tCsca (m^2)" << std::endl;
@@ -228,7 +240,8 @@ int main(int argc, char *argv[])
 			cout << ofile << " --- " << i << " / " << runs.size() << " " << r.refHash << endl;
 			++i;
 
-			const double sizep = 2. * pi * r.aeff / r.lambda;
+			double lambda = rtmath::units::conv_spec("GHz", "um").convert(r.freq);
+			const double sizep = 2. * pi * r.aeff / lambda;
 
 			using namespace rtmath::phaseFuncs;
 			pf_class_registry::inputParamsPartial i;
@@ -236,12 +249,17 @@ int main(int argc, char *argv[])
 			i.aeff_rescale = true;
 			i.aeff_version = pf_class_registry::inputParamsPartial::aeff_version_type::EQUIV_V_SPHERE;
 			i.eps = r.ar;
-			i.m = r.m;
+//std::function<void(std::complex<double>, std::complex<double>, double, std::complex<double> &)> rmeth;
+			std::complex<double> mIce, mAir(1,0), mRes;
+			rtmath::refract::mIce(r.freq, r.temp, mIce);
+			i.vFrac = r.fv;
+			rmeth(mIce, mAir, i.vFrac, mRes);
+			i.m = mRes;
 			i.ref = r.refHash;
 			i.maxDiamFull = r.maxDiamFull;
 			i.rmeth = rmeth; // Yeah, only one refractive index method per program invocation is supported.
 			i.shape = pf_class_registry::inputParamsPartial::shape_type::SPHEROID;
-			i.vFrac = r.fv;
+			
 			i.other = otherOpts;
 
 			mylog("run aeff " << i.aeff << "\n\taeff_rescale " << i.aeff_rescale <<
@@ -267,7 +285,7 @@ int main(int argc, char *argv[])
 			pf_class_registry::setup s;
 			s.beta = 0; s.theta = 0; s.phi = 0;
 			s.sPhi = 0; s.sPhi0 = 0; s.sTheta = 0; s.sTheta0 = 0;
-			s.wavelength = r.lambda;
+			s.wavelength = lambda;
 
 			// smeth is parameter solution-method, which can force a single method to be used
 			p.getCrossSections(s, res, smeth);
@@ -298,14 +316,15 @@ int main(int argc, char *argv[])
 				if (rr.second.Cext < 0 ) Qext = -1;
 				if (vm.count("use-ddscat-conv")) Qbk /= 4.*pi;
 				if (rr.second.Cbk < 0 ) Qbk = -1;
-				out << rr.first << "\t" << r.aeff << "\t" << maxDiam / 1000. << "\t" 
+				out << rr.first << "\t" << tag << "\t" 
+					<< r.aeff << "\t" << maxDiam / 1000. << "\t" 
 					<< Vi << "\t"
-					<< r.freq << "\t" << r.fv << "\t" << effDen << "\t" << r.temp << "\t"
-					<< r.refHash << "\t" << armeth << "\t" << refractScaling << "\t" 
-					<< r.fvMeth << "\t" //<< r.fvMeth * 0 << "\t" // get ice density here.....
-					<< r.ar << "\t" << r.lambda << "\t" << r.m.real() << "\t" << r.m.imag() << "\t"
+					<< r.freq << "\t"
+					<< r.fv << "\t" << effDen << "\t" << r.temp << "\t"
+					<< r.refHash << "\t" << armeth << "\t"
+					//<< r.fvMeth << "\t" //<< r.fvMeth * 0 << "\t" // get ice density here.....
+					<< r.ar << "\t"
 					<< sizep << "\t"
-					<< theta2 << "\t" << beta2 << "\t" << phi2 << "\t"
 					<< rr.second.g << "\t";
 				if (vm.count("show-qs")) {
 					out << Qabs << "\t" << Qbk << "\t" << Qext << "\t" << Qsca << std::endl;
